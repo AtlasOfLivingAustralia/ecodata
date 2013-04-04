c9cdaa2c68bd034f08b99c528c674b1d43970b6e
markew <mark.woolston@axon.com.au>
2013-04-04 01:06:31 +0000

    encapsulates the creation of uuids
    git-svn-id: svn://localhost/trunk/ecodata@7 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ src/groovy/au/org/ala/ecodata/Identifiers.groovy
@@ -0,0 +1,20 @@
+package au.org.ala.ecodata
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mark
+ * Date: 28/03/13
+ * Time: 10:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Identifiers {
+
+    static getNew(useUUID, str) {
+        if (useUUID || !str) {
+            return UUID.randomUUID().toString()
+        }
+        else {
+            return UUID.nameUUIDFromBytes(str as byte[]).toString()
+        }
+    }
+}
