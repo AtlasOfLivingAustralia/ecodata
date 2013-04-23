3b0deac75b96f46335c761d547ef6c62deafc0b3
markew <mark.woolston@axon.com.au>
2013-04-23 00:59:28 +0000

    date parsing
    git-svn-id: svn://localhost/trunk/ecodata@76 63d11371-6d71-872f-44d1-802e8bf8a75e

--- test/unit/au/org/ala/ecodata/ProjectControllerTests.groovy
+++ test/unit/au/org/ala/ecodata/ProjectControllerTests.groovy
@@ -4,13 +4,20 @@ package au.org.ala.ecodata
 
 import grails.test.mixin.*
 
+import java.text.SimpleDateFormat
+
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ProjectController)
 class ProjectControllerTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testDateParsing() {
+        def dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        //def dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ")
+
+        Date date = dateFormat.parse('2013-03-12T14:00:00Z')
+        //Date date = dateFormat.parse('2013-12-03T14:00:00Z')
+        println date
     }
 }
