001ec748aff7cc05f227b5f623e8496393da7d56
markew <mark.woolston@axon.com.au>
2013-06-27 00:59:34 +0000

    add a taglib as we now have formal admin pages
    git-svn-id: svn://localhost/trunk/ecodata@271 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ grails-app/taglib/au/org/ala/ecodata/ECTagLib.groovy
@@ -0,0 +1,30 @@
+package au.org.ala.ecodata
+
+import groovy.xml.MarkupBuilder
+
+class ECTagLib {
+
+    static namespace = "ec"
+
+    /**
+     * @attr active
+     * @attr title
+     * @attr href
+     */
+    def breadcrumbItem = { attrs, body ->
+        def active = attrs.active
+        if (!active) {
+            active = attrs.title
+        }
+        def current = pageProperty(name:'page.pageTitle')?.toString()
+
+        def mb = new MarkupBuilder(out)
+        mb.li(class: active == current ? 'active' : '') {
+            a(href:attrs.href) {
+                i(class:'icon-chevron-right') { mkp.yieldUnescaped('&nbsp;')}
+                mkp.yield(attrs.title)
+            }
+        }
+    }
+
+}
---
+++ test/unit/au/org/ala/ecodata/ECTagLibTests.groovy
@@ -0,0 +1,17 @@
+package au.org.ala.ecodata
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(ECTagLib)
+class ECTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
