57ea9844b84084bb758077e985f7c060efb81006
markew <mark.woolston@axon.com.au>
2013-06-27 01:04:21 +0000

    add bootstrap to support admin pages
    git-svn-id: svn://localhost/trunk/ecodata@274 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,17 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+    app_bootstrap {
+        dependsOn 'jquery'
+        resource url: '/bootstrap/js/bootstrap.min.js'
+        resource url: '/bootstrap/css/bootstrap.min.css'
+        resource url: '/bootstrap/img/glyphicons-halflings-white.png'
+        resource url: '/bootstrap/img/glyphicons-halflings.png'
+    }
+
+    app_bootstrap_responsive {
+        dependsOn 'app_bootstrap'
+        resource url: '/bootstrap/css/bootstrap-responsive.min.css'
+    }
 }
