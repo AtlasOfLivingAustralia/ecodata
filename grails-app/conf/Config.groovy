9cdb975b019dea9dc46af2125be2c43db87f0c73
chrisala <chris.godwin.ala@gmail.com>
2013-09-05 05:13:23 +0000

    Attempting to work around a HTTP header error by bypassing Apache.
    git-svn-id: svn://localhost/trunk/ecodata@641 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -140,7 +140,7 @@ environments {
         grails.logging.jul.usebridge = false
         ecodata.use.uuids = false
         app.external.model.dir = "/data/ecodata/models/"
-        grails.serverURL = "http://ecodata-dev.ala.org.au"
+        grails.serverURL = "http://ecodata-dev.ala.org.au:8080"
         app.uploads.url = grails.serverURL + "/uploads/"
         security.cas.appServerName = grails.serverURL
         security.cas.contextPath = "/" + "ecodata"
