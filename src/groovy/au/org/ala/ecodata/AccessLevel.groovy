f06b18350c0f1f87f4eb9623111bece1c08dc9aa
nickdos <nickdos@gmail.com>
2013-11-20 00:21:08 +0000

    Initial changes for issue #323:	Role and Permission levels. Added caseManager role and admin tab to allocate role. No checks against role yet done.
    git-svn-id: svn://localhost/trunk/ecodata@911 63d11371-6d71-872f-44d1-802e8bf8a75e

--- src/groovy/au/org/ala/ecodata/AccessLevel.groovy
+++ src/groovy/au/org/ala/ecodata/AccessLevel.groovy
@@ -1,16 +1,30 @@
+/*
+ * Copyright (C) 2013 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
 package au.org.ala.ecodata
 
 /**
  * Enum for access levels
  *
  * Note: "starred" might need to be moved as its not a perfect fit here
- * "validator" is not used so could be removed if not required.
  *
  * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
  */
 public enum AccessLevel {
     admin(100),
-    // approver(60),
+    caseManager(60),
     editor(40),
     starred(20)
 
