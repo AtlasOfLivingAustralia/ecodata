5403ce0ebd75487ee96e5ed47dc850a93eaa1349
markew <mark.woolston@axon.com.au>
2013-12-11 06:16:21 +0000

    implemented a white list for IP address for any change to ecodata
    git-svn-id: svn://localhost/trunk/ecodata@1103 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/au/org/ala/ecodata/ApiKeyFilters.groovy
+++ grails-app/conf/au/org/ala/ecodata/ApiKeyFilters.groovy
@@ -6,6 +6,8 @@ class ApiKeyFilters {
 
     def grailsApplication, commonService
 
+    def LOCALHOST_IP = '127.0.0.1'
+
     def filters = {
         apiKeyCheck(controller: '*', action: '*') {
             before = {
@@ -14,8 +16,15 @@ class ApiKeyFilters {
                 def method = controllerClass?.getMethod(actionName?:"index", [] as Class[])
 
                 if (controllerClass?.isAnnotationPresent(RequireApiKey) || method?.isAnnotationPresent(RequireApiKey)) {
-                    if (!commonService.checkApiKey(request.getHeader('Authorization')).valid) {
-                        log.debug "No valid api key for ${controllerName}/${actionName}"
+                    def whiteList = buildWhiteList()
+                    def clientIp = getClientIP(request)
+                    def ipOk = checkClientIp(clientIp, whiteList)
+                    def keyOk = commonService.checkApiKey(request.getHeader('Authorization')).valid
+                    //log.debug "IP ${clientIp} ${ipOk ? 'is' : 'is not'} ok. Key ${keyOk ? 'is' : 'is not'} ok."
+
+                    if (!ipOk || !keyOk) {
+                        log.warning(ipOk ? "No valid api key for ${controllerName}/${actionName}" :
+                            "Non-authorised IP address - ${clientIp}" )
                         response.status = 403
                         def error = [error:'not authorised']
                         render error as JSON
@@ -30,5 +39,33 @@ class ApiKeyFilters {
 
             }
         }
+    }
+
+    /**
+     * Client IP passes if it is in the whitelist of if the whitelist is empty apart from localhost.
+     * @param clientIp
+     * @return
+     */
+    def checkClientIp(clientIp, List whiteList) {
+        whiteList.contains(clientIp) || (whiteList.size() == 1 && whiteList[0] == LOCALHOST_IP)
+    }
+
+    def buildWhiteList() {
+        def whiteList = [LOCALHOST_IP] // allow calls from localhost to make testing easier
+        def config = grailsApplication.config.app.api.whiteList as String
+        if (config) {
+            whiteList.addAll(config.split(',').collect({it.trim()}))
+        }
+        log.debug whiteList
+        whiteList
+    }
+
+    def getClientIP(request) {
+        // External requests to ecodata are proxied by Apache, which uses X-Forwarded-For to identify the original IP.
+        def ip = request.getHeader("X-Forwarded-For")
+        if (!ip) {
+            ip = request.getRemoteHost()
+        }
+        return ip
     }
 }
