d2a788c443a2c587b4f3181b02fef0075c708495
chrisala <chris.godwin.ala@gmail.com>
2014-03-19 04:19:47 +0000

    Updated elasticsearch version.
    git-svn-id: svn://localhost/trunk/ecodata@1418 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
 
         // ElasticSearch
-        compile "org.elasticsearch:elasticsearch:0.90.2"
+        compile "org.elasticsearch:elasticsearch:0.90.12"
 
         // Schema validation for external web service clients
         compile "com.github.fge:json-schema-validator:2.1.6"
