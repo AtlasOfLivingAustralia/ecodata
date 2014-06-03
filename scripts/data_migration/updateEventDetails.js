a00c937fa70a31e6713dd6ec84190f88495b7fee
chrisala <chris.godwin.ala@gmail.com>
2014-06-03 05:23:13 +0000

    Updated the event details migration script.
    git-svn-id: svn://localhost/trunk/ecodata@1593 63d11371-6d71-872f-44d1-802e8bf8a75e

--- scripts/data_migration/updateEventDetails.js
+++ scripts/data_migration/updateEventDetails.js
@@ -1,13 +1,19 @@
-var outputs = db.output.find({name:'Event Details'});
+var outputs = db.output.find({name:'Event Details', 'data.events':{$exists:false}});
+var outputCount = outputs.count();
+var count = 0;
 while (outputs.hasNext()) {
     var output = outputs.next();
+
+    print("Updating event details for activity: "+output.activityId+", output: "+output.outputId);
     output.data.events= [];
 
     var purposes = output.data.eventPurpose;
+
     var hoursPerEvent = 0;
-    if (output.data.eventDurationHrs) {
+    if (output.data.eventDurationHrs && purposes.length > 0) {
         hoursPerEvent = output.data.eventDurationHrs / purposes.length;
     }
+
 
     for (var i=0; i<purposes.length; i++) {
         var event = {};
@@ -19,6 +25,7 @@ while (outputs.hasNext()) {
 
         output.data.events.push(event);
     }
+    output.data.deliveryPartner = "Other (specify in notes)";
 
     delete output.data.eventPurpose;
     delete output.data.eventTopics;
@@ -26,5 +33,11 @@ while (outputs.hasNext()) {
     delete output.data.industryType;
 
     db.output.save(output);
-
+    count++;
+}
+if (outputCount != count) {
+    print("Error! Expected "+outputCount+" but modified "+count+" Event Details outputs");
+}
+else {
+    print("Updated "+count+" Event Details outputs");
 }
