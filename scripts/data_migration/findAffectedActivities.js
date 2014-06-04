903c9b4eadd1478b0de45629c95093c404f3970a
chrisala <chris.godwin.ala@gmail.com>
2014-06-04 02:33:46 +0000

    Working on migration scripts.
    git-svn-id: svn://localhost/trunk/ecodata@1603 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ scripts/data_migration/findAffectedActivities.js
@@ -0,0 +1,71 @@
+var types = [];
+types.push('Feral animal assessment');
+types.push('Infrastructure Establishment');
+types.push('Public Access Management');
+types.push('Vegetation Assessment - BioCondition (QLD)');
+types.push('Vegetation Assessment - BioMetric (NSW)');
+types.push('Vegetation Assessment - Bushland Condition Monitoring (SA)');
+types.push('Vegetation Assessment - Habitat Hectares (VIC)');
+types.push('Vegetation Assessment - Native Vegetation Condition Assessment and Monitoring (WA)');
+types.push('Vegetation Assessment - TasVeg (TAS)');
+types.push('Feral animal assessment');
+
+print('Activity Type, Project ID, Activity ID,Has outputs?');
+for (var i = 0; i<types.length; i++) {
+    var type = types[i];
+    var activities = db.activity.find({type: type});
+    var count = 0;
+    var noOutputCount = 0;
+    while (activities.hasNext()) {
+        var activity = activities.next();
+        var outputs = db.output.find({activityId: activity.activityId}).count();
+        if (outputs > 0) {
+            print(type+','+activity.projectId+','+activity.activityId+',Yes');
+            count++;
+        }
+        else {
+            noOutputCount++;
+
+        }
+    }
+    //print('****' + count + '/' + noOutputCount + '***');
+}
+
+var activities = db.activity.find({type:'Site assessment'});
+while (activities.hasNext()) {
+    var activity = activities.next();
+    var outputs = db.output.find({activityId: activity.activityId}).count();
+    if (outputs > 0) {
+        print(type+','+activity.projectId+','+activity.activityId+',Yes');
+        count++;
+    }
+    else {
+        print(type+','+activity.projectId+','+activity.activityId+',No');
+    }
+}
+
+
+// Pest management with disease component.
+var outputs = db.output.find({name:'Pest Management Details', 'data.pestManagementMethod':/disease/});
+while (outputs.hasNext()) {
+    var output = outputs.next();
+    var activity = db.activity.find({activityId:output.activityId}).next();
+
+    print('Pest and Disease Management,',activity.projectId+','+output.activityId+',Yes');
+}
+
+// Site planning details with deleted output options.
+var outputs = db.output.find({name:'Site Planning Details', 'data.plannedActions.plannedActivityType':/Indigenous|Knowledge Integration and Transfer|Evaluation and Learning|Training and Skills Development|Management Plan Development|Project Administration|Site Monitoring Planning/});
+while (outputs.hasNext()) {
+    var output = outputs.next();
+    var activity = db.activity.find({activityId:output.activityId}).next();
+    print('Site Planning Details,',activity.projectId+','+output.activityId+',Yes');
+}
+
+// accessControlDetails
+var outputs = db.output.find({name:'Access Control Details', 'data.structuresInstalled':{$exists:true}});
+while (outputs.hasNext()) {
+    var output = outputs.next();
+    var activity = db.activity.find({activityId:output.activityId}).next();
+    print('Public Access Management,',activity.projectId+','+output.activityId+',Yes');
+}
--- scripts/data_migration/renameListValues.js
+++ scripts/data_migration/renameListValues.js
@@ -1,5 +1,32 @@
 load('renameListValue.js');
-renameListValue('Revegetation Details', 'data.connectivityIndex', 'Isolated forest or woodland remnant', 'Isolated forest or woodland remnant >1km from other remnants');
-renameListValue('Revegetation Details', 'data.connectivityIndex', 'Isolated grassland', 'Isolated grassland >1km from other remnants');
-renameArrayTypedListValue('Revegetation Details', 'data.environmentalBenefits', 'Habitat restoration - home range improvement', 'Habitat restoration - home range/extent improvement');
-renameArrayTypedListValue('Revegetation Details', 'data.environmentalBenefits', 'Habitat restoration - improved habitat', 'Habitat restoration - habitat condition improvement');
+
+// Admin activities
+renameNestedListValue('Administration Activities', 'adminActions', 'adminActionType', 'Submit project progress / annual/final report', 'Submit project progress / annual / final report');
+
+// Revegetation Details
+renameListValue('Revegetation Details', 'connectivityIndex', 'Isolated forest or woodland remnant', 'Isolated forest or woodland remnant >1km from other remnants');
+renameListValue('Revegetation Details', 'connectivityIndex', 'Isolated grassland', 'Isolated grassland >1km from other remnants');
+renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of more than 1000 ha', 'Patch <1km from a patch of more than 1000ha');
+renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of 100 to 1000 ha', 'Patch <1km from a patch of 100 to 1000ha');
+renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of 25 to 100 ha', 'Patch <1km from a patch of 25 to 100ha');
+renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of less than 25 ha', 'Patch <1km from a patch of less than 25ha');
+
+renameArrayTypedListValue('Revegetation Details', 'environmentalBenefits', 'Habitat restoration - home range improvement', 'Home range / extent improvement');
+renameArrayTypedListValue('Revegetation Details', 'environmentalBenefits', 'Habitat enhancement - improved migration paths', 'Improved habitat connectivity');
+renameArrayTypedListValue('Revegetation Details', 'environmentalBenefits', 'Streambank protection', 'Riparian rehabilitation');
+
+// Site planning details
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Infrastructure Establishment', 'Public Access and Infrastructure');
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Other Conservation Works for Threatened Species', 'Conservation Works for Threatened Species');
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Pest and Disease Management', 'Pest Management');
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Public Access Management', 'Public Access and Infrastructure');
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Fauna (biological) survey', 'Fauna survey - general');
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Flora (biological) survey', 'Flora survey - general');
+renameNestedListValue('Site Planning Details', 'plannedActions', 'plannedActivityType', 'Pest animal assessment', 'Pest Animal Assessment');
+
+// Pest Monitoring Details
+renameNestedListValue('Pest Observation and Monitoring Details', 'pestObservationMonitoringDetails', 'pestPopulationDensityClass', '< 10 individuals / Ha', '< 10 individuals or colonies / Ha');
+renameNestedListValue('Pest Observation and Monitoring Details', 'pestObservationMonitoringDetails', 'pestPopulationDensityClass', '11 - 100 individuals / Ha', '11 - 100 individuals or colonies / Ha');
+renameNestedListValue('Pest Observation and Monitoring Details', 'pestObservationMonitoringDetails', 'pestPopulationDensityClass', '101 - 1000 individuals / Ha', '101 - 1000 individuals or colonies / Ha');
+
+// Plan Management Details
