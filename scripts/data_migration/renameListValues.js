90cec6fd0d7dd969466e8a443130221723f84f00
chrisala <chris.godwin.ala@gmail.com>
2014-06-06 01:42:42 +0000

    Added plan management details changes.
    git-svn-id: svn://localhost/trunk/ecodata@1607 63d11371-6d71-872f-44d1-802e8bf8a75e

--- scripts/data_migration/renameListValues.js
+++ scripts/data_migration/renameListValues.js
@@ -3,14 +3,6 @@ load('renameListValue.js');
 // Admin activities
 renameNestedListValue('Administration Activities', 'adminActions', 'adminActionType', 'Submit project progress / annual/final report', 'Submit project progress / annual / final report');
 
-// Revegetation Details
-renameListValue('Revegetation Details', 'connectivityIndex', 'Isolated forest or woodland remnant', 'Isolated forest or woodland remnant >1km from other remnants');
-renameListValue('Revegetation Details', 'connectivityIndex', 'Isolated grassland', 'Isolated grassland >1km from other remnants');
-renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of more than 1000 ha', 'Patch <1km from a patch of more than 1000ha');
-renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of 100 to 1000 ha', 'Patch <1km from a patch of 100 to 1000ha');
-renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of 25 to 100 ha', 'Patch <1km from a patch of 25 to 100ha');
-renameListValue('Revegetation Details', 'connectivityIndex', 'Connected to patch of less than 25 ha', 'Patch <1km from a patch of less than 25ha');
-
 renameArrayTypedListValue('Revegetation Details', 'environmentalBenefits', 'Habitat restoration - home range improvement', 'Home range / extent improvement');
 renameArrayTypedListValue('Revegetation Details', 'environmentalBenefits', 'Habitat enhancement - improved migration paths', 'Improved habitat connectivity');
 renameArrayTypedListValue('Revegetation Details', 'environmentalBenefits', 'Streambank protection', 'Riparian rehabilitation');
@@ -30,7 +22,7 @@ renameNestedListValue('Pest Observation and Monitoring Details', 'pestObservatio
 renameNestedListValue('Pest Observation and Monitoring Details', 'pestObservationMonitoringDetails', 'pestPopulationDensityClass', '101 - 1000 individuals / Ha', '101 - 1000 individuals or colonies / Ha');
 
 // Plan Management Details
-
+renameListValue('Plan Development Details', 'typeOfPlan', 'Property or Reserve plan', 'Property, Reserve or Site development plan');
 
 // Water management details
 renameArrayTypedListValue('Water Management Details', 'hydrologicalStructuresOnsite', 'Constructed channel', 'Channel - constructed');
