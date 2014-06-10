ce46b6b4aee7a9c5fb639988da45ece8ee1e0361
chrisala <chris.godwin.ala@gmail.com>
2014-06-10 07:12:08 +0000

    Fixed a bug that was incorrectly aborting due to counts when updating arrays that already contain the new value.
    git-svn-id: svn://localhost/trunk/ecodata@1637 63d11371-6d71-872f-44d1-802e8bf8a75e

--- scripts/data_migration/renameListValue.js
+++ scripts/data_migration/renameListValue.js
@@ -26,25 +26,45 @@ function renameListValue(outputName, property, oldValue, newValue) {
 
 function renameArrayTypedListValue(outputName, property, oldValue, newValue) {
 
+    var nestedProperty = property;
     property = 'data.'+property;
     var query = {name:outputName};
     query[property] = oldValue;
     var affectedCount = db.output.find(query).count();
+    query[property] = newValue;
 
     if (affectedCount > 0) {
 
-        var update = {$set:{}};
-        update.$set[property+'.$'] = newValue;
-        db.output.update(query, update, false, true);
-
-        query[property] = newValue;
-        var updatedCount = db.output.find(query).count();
-
-        if (updatedCount != affectedCount) {
-            throw {name:'Error', message:'Updating '+outputName+'.'+property+', expected '+affectedCount+', was '+updatedCount};
+        query[property] = oldValue;
+        var outputs = db.output.find(query);
+        var count = 0;
+        while (outputs.hasNext()) {
+            var output = outputs.next();
+            var prop = output.data[nestedProperty];
+            var newProp = [];
+            for (var i=0; i<prop.length; i++) {
+                if (prop[i] != oldValue) {
+                    newProp.push(prop[i]);
+                }
+                if (newProp.indexOf(newValue) < 0) {
+                    newProp.push(newValue);
+                }
+            }
+            output.data[nestedProperty] = newProp;
+            db.output.save(output);
+            count ++;
         }
 
-        print('Updated '+updatedCount+' outputs. '+outputName+'.'+property+' from '+oldValue+' to '+newValue);
+        query[property] = oldValue;
+        var stillAffectedCount = db.output.find(query).count();
+
+
+        if (stillAffectedCount > 0) {
+            print('Error updating '+outputName+'.'+property+', expected '+affectedCount+', still affected: '+stillAffectedCount);
+            throw {name:'Error', message:'Updating '+outputName+'.'+property+', expected '+affectedCount+', was '+count};
+        }
+
+        print('Updated '+count+' outputs. '+outputName+'.'+property+' from '+oldValue+' to '+newValue);
     }
     else {
         print('No outputs with '+outputName+'.'+property+' = '+oldValue);
