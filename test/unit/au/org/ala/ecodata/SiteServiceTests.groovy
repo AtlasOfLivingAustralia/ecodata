e1b6cc3b6c41d2515bac708452218f181f9f46e4
chrisala <chris.godwin.ala@gmail.com>
2014-07-04 05:01:12 +0000

    Added a site creation from kml function.
    git-svn-id: svn://localhost/trunk/ecodata@1688 63d11371-6d71-872f-44d1-802e8bf8a75e

--- test/unit/au/org/ala/ecodata/SiteServiceTests.groovy
+++ test/unit/au/org/ala/ecodata/SiteServiceTests.groovy
@@ -1,17 +1,11 @@
 package au.org.ala.ecodata
 
-
-
-import grails.test.mixin.*
-import org.junit.*
-
+import grails.test.mixin.TestFor
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(SiteService)
 class SiteServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
-    }
+
 }
