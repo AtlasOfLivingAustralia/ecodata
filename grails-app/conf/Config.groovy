7c178d574f28794e0f8535a4c72d3d784b7794db
chrisala <chris.godwin.ala@gmail.com>
2015-03-31 13:01:01 +1100

    Don't index on startup in dev.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -237,7 +237,7 @@ environments {
         security.cas.appServerName = serverName
         security.cas.contextPath = "/" + appName
         app.uploads.url = "${grails.serverURL}/document/download?filename="
-        app.elasticsearch.indexAllOnStartup = true
+        app.elasticsearch.indexAllOnStartup = false
         app.elasticsearch.indexOnGormEvents = true
     }
     test {
