859c277f353a052574ee2ca456229257d9a6c480
Sathish Sathyamoorthy <sathish.sathyamoorthy@gmail.com>
2015-05-13 11:44:20 +1000

    Moved embedded support to document.

--- grails-app/domain/au/org/ala/ecodata/Project.groovy
+++ grails-app/domain/au/org/ala/ecodata/Project.groovy
@@ -61,8 +61,7 @@ class Project {
     Date lastUpdated
 	String promoteOnHomepage = 'no'
     List activities
-    List iframes
-	
+
     boolean isCitizenScience, isDataSharing
     String projectPrivacy, dataSharingLicense
     String projectType    // survey, works
@@ -164,6 +163,5 @@ class Project {
         dataSharingLicense nullable:true, inList: collectoryLicenseTypes
         userCreated nullable:true
         userLastModified nullable:true
-        iframes nullable:true
     }
 }
