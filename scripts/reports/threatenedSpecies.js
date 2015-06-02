a8abd719e4cde71729ce56b15248e9e1d40367fc
chrisala <chris.godwin.ala@gmail.com>
2015-06-02 11:50:15 +1000

    Updated report to include sub-programme.

--- scripts/reports/threatenedSpecies.js
+++ scripts/reports/threatenedSpecies.js
@@ -1,10 +1,11 @@
-var projects = db.project.find({status:{$ne:'deleted'}, 'custom.details.objectives.rows1.assets':'Threatened Species'});
-//var projects = db.project.find({status:{$ne:'deleted'}, $or:[{'custom.details.objectives.rows1.assets':'Threatened Ecological Communities'}, {'custom.details.objectives.rows1.assets':'Threatened Species'}]});
+//var projects = db.project.find({status:{$ne:'deleted'}, 'custom.details.objectives.rows1.assets':'Threatened Species'});
+var projects = db.project.find({status:{$ne:'deleted'}, $or:[{'custom.details.objectives.rows1.assets':'Threatened Ecological Communities'}, {'custom.details.objectives.rows1.assets':'Threatened Species'}]});
 //var projects = db.project.find({status:{$ne:'deleted'}, 'custom.details.objectives.rows1.assets':'Threatened Ecological Communities'});
 
 //print('Name, Grant ID, External ID, Organisation Name, Programme, Sub-Programme, Planned Start Date, Planned End Date, Funding, Status, Description, State, State, State');
 
 var fundingByRegion = {};
+var programs = [];
 var regions = [];
 var total = 0, count = 0;
 while (projects.hasNext()) {
@@ -22,25 +23,34 @@ while (projects.hasNext()) {
         }
     }
 
+    var programSubProgram = project.associatedProgram+' - '+project.associatedSubProgram;
     if (fundingByRegion[nrm] === undefined) {
         regions.push(nrm);
-        fundingByRegion[nrm] = {count:0, total:0};
+        fundingByRegion[nrm] = {};
+    }
+    if (programs.indexOf(programSubProgram) < 0) {
+        programs.push(programSubProgram);
+    }
+    if (fundingByRegion[nrm][programSubProgram] === undefined) {
+        fundingByRegion[nrm][programSubProgram] = {count:0, total:0};
     }
 
-    fundingByRegion[nrm].count++;
+    fundingByRegion[nrm][programSubProgram].count++;
     count++;
     if (project.custom.details.budget && project.custom.details.budget.overallTotal) {
         total += project.custom.details.budget.overallTotal;
-        fundingByRegion[nrm].total += project.custom.details.budget.overallTotal;
+        fundingByRegion[nrm][programSubProgram].total += project.custom.details.budget.overallTotal;
     }
-
-
 }
 
 
-print('NRM Region,Project Count,Total')
+print('NRM Region,Sub Programme,Project Count,Total')
 for (var i=0; i<regions.length; i++) {
-    print('"'+regions[i]+'",'+fundingByRegion[regions[i]].count+","+fundingByRegion[regions[i]].total);
+    for (var j=0; j<programs.length; j++) {
+        if (fundingByRegion[regions[i]][programs[j]]) {
+            print('"' + regions[i] + '","' + programs[j] + '",' + fundingByRegion[regions[i]][programs[j]].count + "," + fundingByRegion[regions[i]][programs[j]].total);
+        }
+    }
 }
 print("\n");
-print('Totals,'+count+","+total);
+print('Totals,,'+count+","+total);
