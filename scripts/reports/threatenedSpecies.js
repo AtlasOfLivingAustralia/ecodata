ec68bcc00c3f890839311bcb1f3ff2699ee7673f
chrisala <chris.godwin.ala@gmail.com>
2015-07-31 14:40:38 +1000

    Changes to the threatened species/communities script.

--- scripts/reports/threatenedSpecies.js
+++ scripts/reports/threatenedSpecies.js
@@ -4,10 +4,13 @@ var projects = db.project.find({status:{$ne:'deleted'}, $or:[{'custom.details.ob
 
 //print('Name, Grant ID, External ID, Organisation Name, Programme, Sub-Programme, Planned Start Date, Planned End Date, Funding, Status, Description, State, State, State');
 
+
 var fundingByRegion = {};
 var programs = [];
 var regions = [];
 var total = 0, count = 0;
+var subTotal = 0;
+
 while (projects.hasNext()) {
     var project = projects.next();
 
@@ -32,25 +35,55 @@ while (projects.hasNext()) {
         programs.push(programSubProgram);
     }
     if (fundingByRegion[nrm][programSubProgram] === undefined) {
-        fundingByRegion[nrm][programSubProgram] = {count:0, total:0};
+        fundingByRegion[nrm][programSubProgram] = {count:0, total:0, subTotal:0, countByTs:0, countByTEC:0};
     }
 
     fundingByRegion[nrm][programSubProgram].count++;
     count++;
     if (project.custom.details.budget && project.custom.details.budget.overallTotal) {
         total += project.custom.details.budget.overallTotal;
+
         fundingByRegion[nrm][programSubProgram].total += project.custom.details.budget.overallTotal;
+
+        subTotal = 0;
+        for (var i=0; i<project.custom.details.budget.rows.length; i++) {
+            if (project.custom.details.budget.rows[i].shortLabel === 'Communities are protecting species and natural assets') {
+                subTotal += project.custom.details.budget.rows[i].rowTotal;
+            }
+        }
+        fundingByRegion[nrm][programSubProgram].subTotal += subTotal;
     }
+
+    var foundTEC = false, foundTS = false;
+    for (var i=0; i<project.custom.details.objectives.rows1.length; i++) {
+        for (var j=0; j<project.custom.details.objectives.rows1[i].assets.length; j++) {
+            if (project.custom.details.objectives.rows1[i].assets[j] === 'Threatened Ecological Communities') {
+                foundTEC = true;
+            }
+            if (project.custom.details.objectives.rows1[i].assets[j] === 'Threatened Species') {
+                foundTS = true;
+            }
+        }
+    }
+    if (foundTEC) {
+        fundingByRegion[nrm][programSubProgram].countByTEC++;
+    }
+    if (foundTS) {
+        fundingByRegion[nrm][programSubProgram].countByTs++;
+    }
+
+
 }
 
 
-print('NRM Region,Sub Programme,Project Count,Total')
+print('NRM Region,Sub Programme,Project Count,Total,Towards communities are protecting species and natural assests,Projects addressing Threatened Ecological Communities,Projects Addressing Threatened Species')
 for (var i=0; i<regions.length; i++) {
     for (var j=0; j<programs.length; j++) {
         if (fundingByRegion[regions[i]][programs[j]]) {
-            print('"' + regions[i] + '","' + programs[j] + '",' + fundingByRegion[regions[i]][programs[j]].count + "," + fundingByRegion[regions[i]][programs[j]].total);
+            print('"' + regions[i] + '","' + programs[j] + '",' + fundingByRegion[regions[i]][programs[j]].count + "," + fundingByRegion[regions[i]][programs[j]].total +','+ fundingByRegion[regions[i]][programs[j]].subTotal+','+ fundingByRegion[regions[i]][programs[j]].countByTEC+','+ fundingByRegion[regions[i]][programs[j]].countByTs);
         }
     }
 }
 print("\n");
 print('Totals,,'+count+","+total);
+
