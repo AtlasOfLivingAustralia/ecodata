1412e4b26b425b552beb66043e9eba43e1ab21a5
m-r-c <m-r-c@users.noreply.github.com>
2015-10-02 15:55:30 +1000

    Adds name and guid to single sighting to record conversion

--- grails-app/domain/au/org/ala/ecodata/ProjectActivity.groovy
+++ grails-app/domain/au/org/ala/ecodata/ProjectActivity.groovy
@@ -18,12 +18,17 @@ class ProjectActivity {
     List sites = [] // list of sites associated to the survey
     Map visibility
     boolean restrictRecordToSites
+    boolean publicAccess // only editors/admins can add data to a project activity unless publicAccess = true
+    boolean allowPublicViewOfData // allow public users to view data even if the survey is restricted
+
     static constraints = {
-        endDate nullable : true
-        pActivityFormName nullable : true
-        alerts nullable:true
-        sites nullable:true
-        restrictRecordToSites nullable:true
+        endDate nullable: true
+        pActivityFormName nullable: true
+        alerts nullable: true
+        sites nullable: true
+        restrictRecordToSites nullable: true
         visibility nullable: true
+        publicAccess nullable: true
+        allowPublicViewOfData nullable: true
     }
 }
