8ea17eb6890fb0ff286b72562638642e256f27d6
temi <temivarg@gmail.com>
2015-12-09 11:49:39 +1100

    fixes import script error - now species name appear on records page.

--- scripts/misc/createActivitiesForSightingsRecords.js
+++ scripts/misc/createActivitiesForSightingsRecords.js
@@ -21,49 +21,57 @@ while (records.hasNext()) {
         }
     }
 
-    var activity = {
-        projectId:sightingsProject.projectId,
-        projectActivityId:sightingsSurvey.projectActivityId,
-        type:'Single Sighting',
-        status:'active',
-        dateCreated:record.dateCreated,
-        lastUpdated:record.lastUpdated,
-        userId:record.userId,
-        activityId:UUID.generate(),
-        importedFrom:'ecodata-sightings'
-    };
+    var activity = db.activity.find({activityId:record.activityId}).next();
+
+    if(!activity){
+        activity = {
+            type:'Single Sighting',
+            status:'active',
+            dateCreated:record.dateCreated,
+            lastUpdated:record.lastUpdated,
+            userId:record.userId,
+            activityId:UUID.generate(),
+            importedFrom:'ecodata-sightings'
+        };
+    }
+
+    activity.projectId=sightingsProject.projectId;
+    activity.projectActivityId=sightingsSurvey.projectActivityId;
     db.activity.save(activity);
 
-    var output = {
-        activityId:activity.activityId,
-        outputId:UUID.generate(),
-        importedFrom:'ecodata-sightings',
-        name:'Single Sighting',
-        status:'active',
-        dateCreated:record.dateCreated,
-        lastUpdated:record.lastUpdated,
-        data: {
-            eventDate:record.eventDate,
-            multimedia:record.multimedia,
-            recordedBy:record.recordedBy,
-            coordinateUncertaintyInMeters:record.coordinateUncertaintyInMeters,
-            verbatimCoordinates:[record.decimalLongitude, record.decimalLatitude],
-            geodeticDatum:record.geodeticDatum,
-            georeferenceProtocol:record.georeferenceProtocol,
-            identificationVerificationStatus:record.identificationVerificationStatus,
-            individualCount:record.individualCount,
-            locality:record.locality,
-            name:record.scientificName,
-            submissionMethod:record.submissionMethod,
-            imageLicence:record.imageLicence,
-            occurrenceRemarks:record.occurrenceRemarks,
-            dateStr:dateStr,
-            timeStr:timeStr
+    var output = db.output.find({'activityId':record.activityId}).next();
+    if(!output){
+        output = {
+            activityId:activity.activityId,
+            outputId:UUID.generate(),
+            importedFrom:'ecodata-sightings',
+            name:'Single Sighting',
+            status:'active',
+            dateCreated:record.dateCreated,
+            lastUpdated:record.lastUpdated,
+            data: {
+                eventDate:record.eventDate,
+                multimedia:record.multimedia,
+                recordedBy:record.recordedBy,
+                coordinateUncertaintyInMeters:record.coordinateUncertaintyInMeters,
+                locationLongitude:record.decimalLongitude,
+                locationLatitude: record.decimalLatitude,
+                geodeticDatum:record.geodeticDatum,
+                georeferenceProtocol:record.georeferenceProtocol,
+                identificationVerificationStatus:record.identificationVerificationStatus,
+                individualCount:record.individualCount,
+                locality:record.locality,
+                scientificName:record.scientificName,
+                submissionMethod:record.submissionMethod,
+                imageLicence:record.imageLicence,
+                occurrenceRemarks:record.occurrenceRemarks,
+                dateStr:dateStr,
+                timeStr:timeStr
+            }
+        };
+    }
 
-        }
-
-    };
-
+    output.activityId=activity.activityId;
     db.output.save(output);
 
     record.activityId = activity.activityId;
@@ -71,6 +79,8 @@ while (records.hasNext()) {
     record.projectId = sightingsProject.projectId;
     record.verbatimCoordinates = [record.decimalLongitude, record.decimalLatitude]
     record.name = record.scientificName
+    record.status = 'active'
+    record.outputId = output.outputId
 
     db.record.save(record);
 
