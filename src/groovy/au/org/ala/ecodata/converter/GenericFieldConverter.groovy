76cf81050aeb87481ba9f3855b361465418818b5
Javier Molina <javier.molina@acsmail.net.au>
2016-03-03 12:14:28 +1100

    GenericFieldConverter (geoMap converter) must deal with latitudes and longitudes  #475

--- src/groovy/au/org/ala/ecodata/converter/GenericFieldConverter.groovy
+++ src/groovy/au/org/ala/ecodata/converter/GenericFieldConverter.groovy
@@ -9,6 +9,18 @@ class GenericFieldConverter implements RecordFieldConverter {
 
         record.json = (data as JSON).toString()
 
+        Double latitude = getLatitude(data)
+        Double longitude = getLongitude(data)
+
+        // Don't override decimalLongitud or decimalLatitude in case they are null, site info could've already set them
+        if(latitude) {
+            record.decimalLatitude = latitude
+        }
+
+        if(longitude) {
+            record.decimalLongitude = longitude
+        }
+
         Map dwcMappings = extractDwcMapping(metadata)
 
         record << getDwcAttributes(data, dwcMappings)
@@ -19,4 +31,30 @@ class GenericFieldConverter implements RecordFieldConverter {
 
         [record]
     }
+
+
+    private Double getLatitude(Map data) {
+        Double lat = null
+
+        if (data.decimalLatitude) {
+            lat = toDouble(data.decimalLatitude)
+        } else if (data.locationLatitude) {
+            lat = toDouble(data.locationLatitude)
+        }
+
+        lat
+    }
+
+    private Double getLongitude(Map data) {
+        Double lng = null
+
+        if (data.decimalLongitude) {
+            lng = toDouble(data.decimalLongitude)
+        } else if (data.locationLongitude) {
+            lng = toDouble(data.locationLongitude)
+        }
+
+        lng
+    }
+
 }
