0119ee0059fe0b532defd69b590e6608c2c913c2
chrisala <chris.godwin.ala@gmail.com>
2017-03-07 11:02:51 +1100

    Progress commit #325

--- src/groovy/au/org/ala/ecodata/reporting/SummaryXlsExporter.groovy
+++ src/groovy/au/org/ala/ecodata/reporting/SummaryXlsExporter.groovy
@@ -13,7 +13,7 @@ class SummaryXlsExporter {
     def scoreHeaders = ['Category','Title', 'GMS ID','Description','List name','Score Name','Score OutputName', 'Score Units']
     def scoreProperties = ['category','label','gmsId','description','listName','name', 'outputName','units']
 
-    def resultHeaders = ['Category','Group Title','Count','Group','Results','Units']
+    def resultHeaders = ['Category','Title','Count','Group','Results','Units']
     def resultProperties = ['category','groupTitle','count','group','result','units']
 
     def XlsExporter exporter
@@ -26,9 +26,9 @@ class SummaryXlsExporter {
 
         AdditionalSheet sheet = exporter.addSheet('Scores', scoreHeaders)
         int row = sheet.getSheet().lastRowNum
-        results?.each {type ->
-            type?.value?.each{ typeDetail ->
-                sheet.add([typeDetail?.score], scoreProperties, row+1)
+        results?.each {category ->
+            category?.value?.each{ result ->
+                sheet.add([result], scoreProperties, row+1)
                 row = sheet.getSheet().lastRowNum
             }
         }
@@ -36,15 +36,14 @@ class SummaryXlsExporter {
         AdditionalSheet resultsSheet = exporter.addSheet('Results', resultHeaders)
         int resultRow = resultsSheet.getSheet().lastRowNum
         results?.each {category ->
-            category?.value?.each{ type ->
-                type?.results?.each{ item ->
-                    def customOutput = [category: category.key, groupTitle:type.groupTitle,count:item.count, group:item.group, result:item.result,units:item.units]
-                    if(customOutput.result instanceof groovy.lang.MapWithDefault) {
-                        customOutput.result = customOutput.result as String
-                    }
-                    resultsSheet.add([customOutput], resultProperties, resultRow + 1)
-                    resultRow = resultsSheet.getSheet().lastRowNum
+            category?.value?.each{ result ->
+                def customOutput = [category: category.key, groupTitle:result.label,count:result.result.count, group:result.result.group, result:result.result,units:result.units]
+                if(customOutput.result instanceof groovy.lang.MapWithDefault) {
+                    customOutput.result = customOutput.result as String
                 }
+                resultsSheet.add([customOutput], resultProperties, resultRow + 1)
+                resultRow = resultsSheet.getSheet().lastRowNum
+
             }
         }
     }
