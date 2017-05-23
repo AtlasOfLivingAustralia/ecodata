241503234cf863b45c009505f8d669a63a752540
chrisala <chris.godwin.ala@gmail.com>
2017-05-24 09:09:18 +1000

    Fixed collectory organisation creation.

--- test/unit/au/org/ala/ecodata/CollectoryServiceSpec.groovy
+++ test/unit/au/org/ala/ecodata/CollectoryServiceSpec.groovy
@@ -2,6 +2,7 @@ package au.org.ala.ecodata
 
 import grails.converters.JSON
 import grails.test.mixin.TestFor
+import org.codehaus.groovy.grails.commons.InstanceFactoryBean
 import org.codehaus.groovy.grails.web.converters.marshaller.json.CollectionMarshaller
 import org.codehaus.groovy.grails.web.converters.marshaller.json.MapMarshaller
 import spock.lang.Specification
@@ -29,6 +30,25 @@ class CollectoryServiceSpec extends Specification {
         JSON.registerObjectMarshaller(new CollectionMarshaller())
 
     }
+
+    void "new organisations will be registered with the collectory"() {
+        setup:
+        Map organisation = [name:'org1', orgType:'other', description: 'org1 description', url:'http://www.org1.org', acronym: 'o1']
+        Map expected =  ['institutionType':organisation.orgType, name:organisation.name, pubDescription: organisation.description, websiteUrl:organisation.url, acronym:organisation.acronym]
+        Map actual = null
+
+        when:
+        service.createInstitution(organisation)
+
+        then:
+
+        1 * webServiceMock.doPost(collectoryBaseUrl+'ws/institution', _) >> {args -> actual = args[1]}
+        actual == expected
+        1 * webServiceMock.extractIdFromLocationHeader(_) >> ''
+        0 * _
+
+    }
+
 
     void "new projects will be registered with the collectory"() {
         setup:
@@ -106,6 +126,23 @@ class CollectoryServiceSpec extends Specification {
         then:
         0 * webServiceMock.doPost(_, _)
 
+    }
+
+    void "the institution id will be mapped if an associated organisation has one"() {
+        setup:
+        String dataResourceId = 'dr1234'
+        String institutionId = 'dr2345'
+        Organisation.metaClass.static.findByOrganisationIdAndStatusNotEqual = {String orgId, String status -> [collectoryInstitutionId:institutionId]}
+
+        Map projectData = [name:'project 1', description:'test 123', isMERIT:false, dataResourceId:dataResourceId, organisationId:'1234']
+
+        when:
+        service.updateDataResource(projectData)
+
+        then:
+
+        1 * webServiceMock.doPost(collectoryBaseUrl+"ws/dataResource/"+dataResourceId,  [name:projectData.name, pubDescription:projectData.description, institution:[uid:institutionId], hiddenJSON:[isMERIT:false, dataResourceId: dataResourceId, organisationId: '1234']]) >> [:]
+        0 * webServiceMock.doPost(_, _)
     }
 
 }
