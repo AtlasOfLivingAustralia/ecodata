6015be11d345a6d07aebdf2168d8f76f13268f32
Qifeng <qifeng.bai@csiro.au>
2018-01-17 16:41:46 +1100

    log added to update script

--- scripts/data_migration/updateAllowPloygonsForExistingRecords.js
+++ scripts/data_migration/updateAllowPloygonsForExistingRecords.js
@@ -3,29 +3,57 @@
 * Qifeng Bai
 *  
 */
-//This script is used to set two new fields: allowPolygons and allowPoints to the existing records
-//For the existing records which allowAddtionalSurverySites is true, the set allowPolygons to true and allowPoints to false
-// {allowPloygons:{$exists:false}} is used to double check, in case some existing surveies have been mannually updated.
+//1, This script is used to set two new fields: allowPolygons and allowPoints to the existing records
+// For the existing records which allowAddtionalSurverySites is true, the set allowPolygons to true and allowPoints to true
+
+//2, update projectActivities which allowAddtionalSurveySites does not exist.
 
 function updateAllowPolygonsForProjectActviity(){
-    db.getCollection('projectActivity').update({$and:[{allowAdditionalSurveySites:true},{allowPolygons:{$exists:false}}]},{$set:{allowPolygons:true,allowPoints:false}},{multi:true})
+
+    print('Update allowPolygons  to false, and allowPoints to true, if allowAdditionalSurveySites is not defined')
+    var cPA_N =  db.projectActivity.find({allowAdditionalSurveySites:{$exists:false}})
+    cPA_N.forEach(function(pa){
+        print('updating project activity without allowAddtionalSurveySites: '+ pa.projectActivityId)
+        })
+        
+    print('Update allowPolygons and allowPoints to true, if allowAdditionalSurveySites is true')
+    var cPA_N =  db.projectActivity.find({allowAdditionalSurveySites:true})
+    cPA_N.forEach(function(pa){
+        print('updating project activity with allowAddtionSurveySites is true: '+ pa.projectActivityId)
+        })   
+    
+    db.projectActivity.update({allowAdditionalSurveySites:{$exists:false}},{$set:{allowPolygons:false,allowPoints:true,allowAdditionalSurveySites:false}},{multi:true})    
+    db.projectActivity.update({allowAdditionalSurveySites:true},{$set:{allowPolygons:true,allowPoints:true}},{multi:true})
 }
 
 //Found some activities which has  both location and lat/lng of this projectActivivity
 function removeSiteIdForTheProjectActivity(pai){
-    var cAct = db.activity.find({projectActivityId:pai})
-    while (cAct.hasNext()){
-        var activity = cAct.next()    
-        var outputs = db.output.find({activityId:activity.activityId})
-        outputs.forEach(function(output){
-            if (output.data){
-                 if (output.data.location && output.data.locationLongitude && output.data.locationLatitude){                     
-                     print("remove location from: " +output.activityId)                     
-                     db.output.update({outputId:output.outputId},{$unset:{'data.location':""}}) 
-                 }
-            }          
-        })
+    var pId= (db.getCollection('projectActivity').findOne({projectActivityId:'59754a0b-478c-4815-b368-eef5f9edfb07'})).projectId;
+    var project = db.project.findOne({projectId:pId})
+    var psid = project.projectSiteId
+    if (psid){
+            print('Found project site id: '+ psid)
+            var cAct = db.activity.find({projectActivityId:pai})
+            while (cAct.hasNext()){
+                var activity = cAct.next()    
+                var outputs = db.output.find({activityId:activity.activityId})              
+                outputs.forEach(function(output){
+                    if (output.data){
+                         if (output.data.location && output.data.locationLongitude && output.data.locationLatitude){ 
+                                if(psid == output.data.location){
+                                        print("remove location from activity: " +output.activityId)                     
+                                        db.output.update({outputId:output.outputId},{$unset:{'data.location':""}})                                        
+                                    }                            
+                         }
+                    }          
+                })
+            }
+    }else{
+        print('Error: SiteId is not found for project activity: '+pai)
     }
+
+
 }
 
-removeSiteIdForTheProjectActivity('59754a0b-478c-4815-b368-eef5f9edfb07')
+updateAllowPolygonsForProjectActviity()
+//removeSiteIdForTheProjectActivity('59754a0b-478c-4815-b368-eef5f9edfb07')
