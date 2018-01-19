80f850ceee6899ec1c0afba41fd91eff097e01dc
Qifeng <qifeng.bai@csiro.au>
2018-01-19 13:29:34 +1100

    fix location in migration script

--- scripts/data_migration/updateAllowPloygonsForExistingRecords.js
+++ scripts/data_migration/updateAllowPloygonsForExistingRecords.js
@@ -10,8 +10,8 @@
 
 function updateAllowPolygonsForProjectActviity(){
 
-    print('Update allowPolygons  to false, and allowPoints to true, if allowAdditionalSurveySites is not defined')
-    var cPA_N =  db.projectActivity.find({allowAdditionalSurveySites:{$exists:false}})
+    print('Update allowPolygons  to false, and allowPoints to true, if allowAdditionalSurveySites is not defined/false')
+    var cPA_N =  db.projectActivity.find({$or:[{allowAdditionalSurveySites:{$exists:false}}, {allowAdditionalSurveySites:false}]})
     cPA_N.forEach(function(pa){
         var project = db.project.findOne({projectId:pa.projectId});
         print('updating project activity without allowAddtionalSurveySites: ' + pa.name +" : " + pa.projectActivityId + " for project:" + project.name +" : " +project.projectId)
@@ -24,7 +24,7 @@ function updateAllowPolygonsForProjectActviity(){
         print('updating project activity with allowAddtionSurveySites is true: '+ pa.name +" : " +  pa.projectActivityId + " for project:" + project.name +" : " +project.projectId)
         })   
     
-    db.projectActivity.update({allowAdditionalSurveySites:{$exists:false}},{$set:{allowPolygons:false,allowPoints:true,allowAdditionalSurveySites:false}},{multi:true})    
+    db.projectActivity.update({$or:[{allowAdditionalSurveySites:{$exists:false}}, {allowAdditionalSurveySites:false}]},{$set:{allowPolygons:false,allowPoints:true,allowAdditionalSurveySites:false}},{multi:true})    
     db.projectActivity.update({allowAdditionalSurveySites:true},{$set:{allowPolygons:true,allowPoints:true}},{multi:true})
 }
 
@@ -57,5 +57,33 @@ function removeSiteIdForTheProjectActivity(pai){
 
 }
 
-updateAllowPolygonsForProjectActviity()
+function findOutputsWithBoth(){
+    var projects =  db.project.find({isMERIT:false})
+    var total = 0;
+    projects.forEach(function(project){
+        var projectId = project.projectId;
+        var activites = db.activity.find({projectId : projectId})
+        var outputsInproject = 0;
+        activites.forEach(function(activity){
+            var activityId = activity.activityId;
+            var outputs = db.output.find({$and:[{activityId: activityId}, {'data.locationLatitude':{$ne:null}} , {'data.locationLongitude':{$ne:null}}, {'data.location':{$ne:null} }]})
+            var oc = outputs.count();
+            if (oc > 0){
+               total = total+oc;
+               outputsInproject += oc;
+               db.output.update({$and:[{activityId: activityId}, {'data.locationLatitude':{$ne:null}} , {'data.locationLongitude':{$ne:null}}, {'data.location':{$ne:null} }]},{$unset:{'data.location':""}})
+            }
+        })
+
+        if (outputsInproject>0){
+            print (project.name  +'\t' + "https://biocollect.ala.org.au/project/index/"+projectId + '\t' + outputsInproject )
+        }
+    })
+}
+
+
+//updateAllowPolygonsForProjectActviity()
+findOutputsWithBoth()
+print ('Excution is done')
+
 //removeSiteIdForTheProjectActivity('59754a0b-478c-4815-b368-eef5f9edfb07')
