a5c825fc55948cfa32d5db1ba8cd5076bc1e8be8
chrisala <chris.godwin.ala@gmail.com>
2018-01-30 15:16:58 +1100

    Allow spreadsheet changes to be merged into the model.

--- grails-app/assets/javascripts/admin.js
+++ grails-app/assets/javascripts/admin.js
@@ -1,7 +1,9 @@
 
 //= require application
+//= require underscorejs/1.8.3/underscore
 //= require bootstrap-datepicker/js/bootstrap-datepicker
 //= require activityModel
 //= require outputModel
 //= require programModel
 //= require attach-document-no-ui
+
