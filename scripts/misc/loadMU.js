a848602a6d6daabc8ab8bdd8e02d692514819b21
chrisala <chris.godwin.ala@gmail.com>
2018-06-19 12:55:40 +1000

    Another rework of the services #1343

--- scripts/misc/loadMU.js
+++ scripts/misc/loadMU.js
@@ -271,7 +271,13 @@ var program = {
     }
 };
 
-db.program.insert(program);
+var rlpProgram = db.program.find({name:program.name});
+if (rlpProgram.hasNext()) {
+    var rlp2 = rlpProgram.next();
+    program._id = rlp2._id;
+    program.programId = rlp2.programId;
+}
+db.program.save(program);
 
 var parentId = db.program.find({programId:program.programId}).next()._id;
 
@@ -290,7 +296,7 @@ for (var i=0; i<mus.length; i++) {
                 {
                     "category":"Outputs Reporting",
                     "reportType": "Activity",
-                    "activityType": "RLP Progress Report",
+                    "activityType": "RLP Output Report",
                     "reportNameFormat": "Outputs Report %d",
                     "reportDescriptionFormat": "Outputs Report %d for %4$s"
                 },
@@ -385,6 +391,16 @@ for (var i=0; i<mus.length; i++) {
         {outcome:"By 2023, there is an increase in the capacity of agriculture systems to adapt to significant changes in climate and market demands for information on provenance and sustainable production.", priorities:[{category:"Sustainable Agriculture"}], category:"agriculture"}
     ];
 
-    db.program.insert(mu);
+
+    var existingMu = db.program.find({name:mu.name});
+    if (existingMu.hasNext()) {
+        var mu2 = existingMu.next();
+        mu.description = mu2.description;
+        mu._id = mu2._id;
+        mu.programId = mu2.programId;
+
+    }
+
+    db.program.save(mu);
 
 }
