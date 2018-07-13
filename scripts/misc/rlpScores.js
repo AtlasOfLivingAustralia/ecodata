8c251472cfc86faa38c638f9691fcaf5e7a0f9db
chrisala <chris.godwin.ala@gmail.com>
2018-07-13 13:36:15 +1000

    Added fire maangement score.

--- scripts/misc/rlpScores.js
+++ scripts/misc/rlpScores.js
@@ -527,6 +527,27 @@ scores = [
    {
       "configuration": {
          "filter": {
+            "filterValue": "RLP - Fire management",
+            "property": "name",
+            "type": "filter"
+         },
+         "childAggregations": [{
+            "property": "data.areaProtectedHa",
+            "type": "SUM"
+         }],
+         "label": "Area (ha) protected by fire management action"
+      },
+      "outputType": "RLP - Fire management",
+      "entityTypes": ["RLP Output Report"],
+      "label": "Area (ha) protected by fire management action",
+      "units": "",
+      "category": "RLP",
+      "isOutputTarget": true,
+      "status": "active"
+   },
+   {
+      "configuration": {
+         "filter": {
             "filterValue": "RLP - Flora survey",
             "property": "name",
             "type": "filter"
