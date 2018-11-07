375f61615dcb6db8ce310e96f912997dfd5194b4
Chris <chris.godwin.ala@gmail.com>
2018-11-07 14:37:07 +1100

    Added some defaults to the dev config.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -623,6 +623,12 @@ environments {
         grails.hostname = "devt.ala.org.au"
         app.elasticsearch.indexAllOnStartup = false
         app.elasticsearch.indexOnGormEvents = true
+        app.external.model.dir = "./models/"
+        grails.serverURL = "http://devt.ala.org.au:8080/ecodata"
+        app.uploads.url = "${grails.serverURL}/document/download?filename="
+        grails.mail.host="localhost"
+        grails.mail.port=1025
+        temp.dir="/data/ecodata/tmp"
     }
     test {
         // Override disk store so the travis build doesn't fail.
