264b227d1b0f5264842a254ee160303b6ead67cb
chrisala <chris.godwin.ala@gmail.com>
2019-04-16 13:25:51 +1000

    Don't set userdetails with no userId

--- grails-app/conf/au/org/ala/ecodata/AuditFilters.groovy
+++ grails-app/conf/au/org/ala/ecodata/AuditFilters.groovy
@@ -14,15 +14,18 @@ class AuditFilters {
                 // userId is set from either the request param userId or failing that it tries to get it from
                 // the UserPrincipal (assumes ecodata is being accessed directly via admin page)
                 def userId = request.getHeader(grailsApplication.config.app.http.header.userId)?:RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.userid
-                def userDetails = userService.setCurrentUser(userId)
-                if (userDetails) {
-                    // We set the current user details in the request scope because
-                    // the 'afterView' hook can be called prior to the actual rendering (despite the name)
-                    // and the thread local can get clobbered before it is actually required.
-                    // Consumers who have access to the request can simply extract current user details
-                    // from there rather than use the service.
-                    request.setAttribute(UserDetails.REQUEST_USER_DETAILS_KEY, userDetails)
+                if (userId) {
+                    def userDetails = userService.setCurrentUser(userId)
+                    if (userDetails) {
+                        // We set the current user details in the request scope because
+                        // the 'afterView' hook can be called prior to the actual rendering (despite the name)
+                        // and the thread local can get clobbered before it is actually required.
+                        // Consumers who have access to the request can simply extract current user details
+                        // from there rather than use the service.
+                        request.setAttribute(UserDetails.REQUEST_USER_DETAILS_KEY, userDetails)
+                    }
                 }
+
             }
             after = { Map model ->
 
