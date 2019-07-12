18ba6d2a3dcac1e2b9bce7502a1a4bb79fccdc70
Chris <chris.godwin.ala@gmail.com>
2019-07-13 09:53:17 +1000

    Slightly improved accidentally committed test. #540

--- test/unit/au/org/ala/ecodata/ProjectControllerSpec.groovy
+++ test/unit/au/org/ala/ecodata/ProjectControllerSpec.groovy
@@ -1,15 +1,17 @@
 package au.org.ala.ecodata
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 @TestFor(ProjectController)
+@Mock(Project)
 class ProjectControllerSpec extends Specification {
 
     ProjectService projectService
 
     def setup() {
-        projectService = Mock(projectService)
+        projectService = Mock(ProjectService)
         controller.projectService = projectService
     }
 
@@ -17,11 +19,20 @@ class ProjectControllerSpec extends Specification {
     def "clients can request different views of project data according to their needs"() {
         setup:
         String projectId = 'p1'
+        Project p = new Project(projectId:projectId, name:"Project 1")
+        p.save()
 
         when:
         controller.get(projectId)
 
         then:
-        true == true
+        1 * projectService.toMap(p, [], false, null)
+
+        when:
+        params.view = 'all'
+        controller.get(projectId)
+
+        then:
+        1 * projectService.toMap(p, ProjectService.ALL, false, null)
     }
 }
