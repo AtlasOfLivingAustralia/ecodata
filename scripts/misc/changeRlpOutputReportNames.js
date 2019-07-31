47a1335573a5991c29f7a8ef63614aa08e87dc77
Chris <chris.godwin.ala@gmail.com>
2019-07-31 15:36:08 +1000

    Config update script for #1845

---
+++ scripts/misc/changeRlpOutputReportNames.js
@@ -0,0 +1,17 @@
+var programs = db.program.find({parent:{$exists:true}});
+while (programs.hasNext()) {
+    var program = programs.next();
+
+    if (program.config && program.config.projectReports) {
+        for (var i=0; i<program.config.projectReports.length; i++) {
+            if (program.config.projectReports[i].activityType == 'RLP Output Report') {
+                program.config.projectReports[i].reportNameFormat = "%5\$s - Outputs Report %7\$02d";
+                program.config.projectReports[i].reportDescriptionFormat = "%5\$s - Outputs Report %7\$02d";
+            }
+        }
+        db.program.save(program);
+    }
+    else {
+        print("***************************Error: no config for program "+program.programId);
+    }
+}
