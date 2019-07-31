4ec57bdd55332f9f9b4afa7b243a18c609a649e8
Chris <chris.godwin.ala@gmail.com>
2019-07-31 15:45:54 +1000

    Added reporting period to the report name #1845

--- scripts/misc/changeRlpOutputReportNames.js
+++ scripts/misc/changeRlpOutputReportNames.js
@@ -5,8 +5,8 @@ while (programs.hasNext()) {
     if (program.config && program.config.projectReports) {
         for (var i=0; i<program.config.projectReports.length; i++) {
             if (program.config.projectReports[i].activityType == 'RLP Output Report') {
-                program.config.projectReports[i].reportNameFormat = "%5\$s - Outputs Report %7\$02d";
-                program.config.projectReports[i].reportDescriptionFormat = "%5\$s - Outputs Report %7\$02d";
+                program.config.projectReports[i].reportNameFormat = "%5$s - %6$s Outputs Report %7$02d";
+                program.config.projectReports[i].reportDescriptionFormat = "%5$s - %6$s Outputs Report %7$02d";
             }
         }
         db.program.save(program);
