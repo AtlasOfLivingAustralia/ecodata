575e650c8c22abfdedfacfa5d3cb16c606e93d85
Chris <chris.godwin.ala@gmail.com>
2019-08-29 10:01:54 +1000

    Set source/target level to try and workaround javac 9

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,8 @@ grails.servlet.version = "2.5" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-
+grails.project.target.level = 1.8
+grails.project.source.level = 1.8
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 //grails.server.port.http = 8079
 
