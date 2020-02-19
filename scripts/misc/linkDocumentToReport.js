55ef883f9c07467d4ee78e040a58c0c5725a7f90
Qifeng <qifeng.bai@csiro.au>
2020-02-19 15:41:56 +1100

    scriping to update document stage to report id

--- scripts/misc/linkDocumentToReport.js
+++ scripts/misc/linkDocumentToReport.js
@@ -2,15 +2,37 @@
 
 // Work on RLP project
 // projects which valid programId
-db.getCollection('project').find({programId:{$exists:true}})
+//db.getCollection('project').find({programId:{$exists:true}})
 
 // find reports list based on projectId and activityType
 // order by toDate ASC
 // Get name and reportId
 
-db.getCollection('report').find({$and:[{projectId:'97a57212-1e53-4913-9e39-9771b6f1f0cc'}, {activityType:'RLP Output Report'}]}).sort({toDate:1})
+//db.getCollection('report').find({$and:[{projectId:'97a57212-1e53-4913-9e39-9771b6f1f0cc'}, {activityType:'RLP Output Report'}]}).sort({toDate:1})
 
 // Find documments which have projectID
 // update stage number with report name in the same position
 // update to reportId
+
+//db.getCollection('document').find({projectId:'97a57212-1e53-4913-9e39-9771b6f1f0cc'})
+
+db = db.getSiblingDB('ecodata')
+var projects = db.getCollection('project').find({programId:{$exists:true}})
+
+projects.forEach(function(project){
+    var projectId = project.projectId;
+
+    var reports = db.getCollection('report').find({$and:[{projectId: projectId}, {activityType:'RLP Output Report'}]}).sort({toDate:1}).toArray()
+     var documents = db.document.find({projectId:projectId}).toArray()
+
+    documents.forEach(function(document){
+        if(document['stage'] && document['stage']!='undefined'){
+            var documentId = document.documentId
+            var stage = document['stage']
+            print(documentId + ' ' + stage + ' ' + reports[stage-1].name )
+            db.document.update({documentId: documentId},{$set:{stage_check: stage,stage:reports[stage-1].name, reportId:reports[stage-1].reportId}})
+        }
+    })
+  }
+)
 
