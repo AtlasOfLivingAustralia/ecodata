e58a4dc34931c6fa95845f237dd1e72ce5e9638d
Qifeng <qifeng.bai@csiro.au>
2020-02-21 14:45:26 +1100

    fix bugs in scripting update org

--- scripts/misc/updateProjectOrg.js
+++ scripts/misc/updateProjectOrg.js
@@ -91,15 +91,41 @@ for(var i=0; i<manual_check_data.length;i++){
     var ABNName = manual_check_data[i].ABNName
     for(var j=0; j < manual_check_data[i].projects.length; j++){
         var project = manual_check_data[i].projects[j]
-        if(!project.igored){
+        if(!project.ignored){
             db.project.update({$and:[{projectId: project.projectId },{status:{$ne:'deleted'}}]},{$set: {'organisationName':ABNName}})
         }
     }
 }
 */
 
+/*
+var orgsInProject = db.project.distinct('organisationId',{$and: [{"organisationId":{$ne:""}}, {isMERIT: true}]})
 
-Papa.parse(data, {
+orgsInProject.forEach(function(orgId){
+    var project = db.project.findOne({$and:[{organisationId: orgId },{isMERIT:true}]})
+    var orgName = project.organisationName
+    db.organisation.update({organisationId: orgId},{$set:{organisationName:orgName}})
+})*/
+
+
+//Fixing roll back of type ignore
+
+for(var i=0; i<manual_check_data.length;i++){
+    var ABNName = manual_check_data[i].ABNName
+    for(var j=0; j < manual_check_data[i].projects.length; j++){
+        var project = manual_check_data[i].projects[j]
+        if(project.ignored){
+            //print('replace : '+ project.projectId +' - '+ ABNName +' with original org: ' + project.organisationName )
+            print(project.projectId +','+ABNName +',' + project.organisationName )
+            //db.project.update({$and:[{projectId: project.projectId },{status:{$ne:'deleted'}}]},{$set: {'organisationName':project.organisationName}})
+        }
+    }
+}
+
+//
+
+
+/*Papa.parse(data, {
     delimiter: "\t",
     header: true,
     complete: function (result) {
@@ -113,15 +139,39 @@ Papa.parse(data, {
             db.organisation.update({name: targetOrg},{$set: {'name':ABNName, 'ABN':abn}})
         }
     }
-})
+})*/
+
+
+
 
 
 
 /*
-var orgsInProject = db.project.distinct('organisationId',{$and: [{"organisationId":{$ne:""}}, {isMERIT: true}]})
-
-orgsInProject.forEach(function(orgId){
-    var project = db.project.findOne({$and:[{organisationId: orgId },{isMERIT:true}]})
-    var orgName = project.organisationName
-    db.organisation.update({organisationId: orgId},{$set:{organisationName:orgName}})
-})*/
+replace : eb09065e-9046-4b48-a81e-47328b8459d3 - ACT Environment and Planning Directorate - Departmental with original org: Department of Primary Industries, Parks, Water and Environment
+replace : 985bfa20-3d8d-452e-a78b-7d1442b80a71 - ACT Environment and Planning Directorate - Departmental with original org: Department of Environment and Natural Resources
+replace : ad20a84f-c912-47cd-bfdb-94d9b51b0abe - ACT Environment and Planning Directorate - Departmental with original org: Department of Environment and Natural Resources
+replace : 5de45610-ab8f-40ff-84ab-68293ac7ed01 - ACT Environment and Planning Directorate - Departmental with original org: Department of Environment and Natural Resources
+replace : da74289f-b7b8-4a8c-b688-394f955b2df4 - ACT Environment and Planning Directorate - Departmental with original org: WA Department of Parks and Wildlife
+replace : aa37abc4-36dc-47c4-9d83-093b41aca0a7 - ACT Environment and Planning Directorate - Departmental with original org: WA Department of Parks and Wildlife
+replace : 878acdc0-7f89-45e2-b412-83453a29359b - THE AUSTRALIAN INSTITUTE OF MARINE SCIENCE with original org: Great Barrier Reef Foundation
+replace : eb09065e-9046-4b48-a81e-47328b8459d3 - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: Department of Primary Industries, Parks, Water and Environment
+replace : da74289f-b7b8-4a8c-b688-394f955b2df4 - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: WA Department of Parks and Wildlife
+replace : aa37abc4-36dc-47c4-9d83-093b41aca0a7 - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: WA Department of Parks and Wildlife
+replace : 1757b778-33ac-452f-b139-77d5c038bc99 - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: ACT Environment and Planning Directorate - Departmental
+replace : bd4e78ec-a2f5-4be5-a9a5-652d54fec890 - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: Department of Environment and Natural Resources
+replace : 206ba979-06a9-4981-a605-7c1f355cfc5b - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: NQ Dry Tropics Ltd
+replace : ff57ca4c-2ecc-4fae-9154-258e31b5b3b2 - DEPARTMENT OF ENVIRONMENT AND NATURAL RESOURCES with original org: Great Barrier Reef Marine Park Authority
+replace : ef0592b6-ed76-46eb-ac00-174069686547 - Department of Primary Industries Parks Water and Environment with original org: Fowlers Bay Eco Park Pty Ltd
+replace : 534f4fdf-e12c-4a81-bde5-3c76889b211c - Department of Primary Industries Parks Water and Environment with original org: Greening Australia Limited
+replace : 30a7a147-8479-4691-aa61-d773354784b0 - Department of Primary Industries Parks Water and Environment with original org: Great Barrier Reef Marine Park Authority
+replace : a1c91a60-6c1c-4caa-9346-63871e4f521a - Department of Primary Industries Parks Water and Environment with original org: Fitzroy Basin Association Inc.
+replace : 1856d8ef-886b-465f-b4a8-19eb46cfd669 - FOWLERS BAY ECO PARK PTY LTD with original org: Department of Primary Industries, Parks, Water and Environment
+replace : 534f4fdf-e12c-4a81-bde5-3c76889b211c - FOWLERS BAY ECO PARK PTY LTD with original org: Greening Australia Limited
+replace : 30a7a147-8479-4691-aa61-d773354784b0 - FOWLERS BAY ECO PARK PTY LTD with original org: Great Barrier Reef Marine Park Authority
+replace : a1c91a60-6c1c-4caa-9346-63871e4f521a - FOWLERS BAY ECO PARK PTY LTD with original org: Fitzroy Basin Association Inc.
+replace : 5f92de26-1090-49c4-a1f5-33cf4f8fd16f - GREAT BARRIER REEF FOUNDATION with original org: Australian Institute of Marine Science
+replace : bd4e78ec-a2f5-4be5-a9a5-652d54fec890 - GREAT BARRIER REEF MARINE PARK AUTHORITY with original org: Department of Environment and Natural Resources
+replace : 206ba979-06a9-4981-a605-7c1f355cfc5b - GREAT BARRIER REEF MARINE PARK AUTHORITY with original org: NQ Dry Tropics Ltd
+replace : ef0592b6-ed76-46eb-ac00-174069686547 - GREENING AUSTRALIA with original org: Fowlers Bay Eco Park Pty Ltd
+replace : 1856d8ef-886b-465f-b4a8-19eb46cfd669 - GREENING AUSTRALIA with original org: Department of Primary Industries, Parks, Water and Environment
+replace : a1c91a60-6c1c-4caa-9346-63871e4f521a - GREENING AUSTRALIA with original org: Fitzroy Basin Association Inc.*/
