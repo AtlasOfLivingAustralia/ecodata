9fa2df5e1f7da6da85bce7511fa27f1853fc8795
matilda2812 <matildastevenson2812@gmail.com>
2020-03-19 14:55:57 +1100

    Use specified image licence

--- src/groovy/au/org/ala/ecodata/converter/ImageConverter.groovy
+++ src/groovy/au/org/ala/ecodata/converter/ImageConverter.groovy
@@ -2,7 +2,7 @@ package au.org.ala.ecodata.converter
 
 class ImageConverter implements RecordFieldConverter {
 
-    private static final String DEFAULT_RIGHTS_STATEMENT = "The rights to all uploaded images are held equally, under a Creative Commons Attribution (CC-BY v3.0) license, by the contributor of the image and the primary organisation responsible for the project to which they are contributed."
+    private static final String DEFAULT_RIGHTS_STATEMENT = "The rights to all uploaded images are held under the specified Creative Commons license, by the contributor of the image and the primary organisation responsible for the project to which they are contributed."
     private static final String DEFAULT_LICENCE = "Creative Commons Attribution"
 
     List<Map> convert(Map data, Map metadata = [:]) {
@@ -18,7 +18,7 @@ class ImageConverter implements RecordFieldConverter {
                 it.rightsHolder = it.creator ?: it.attribution
                 it.rights = DEFAULT_RIGHTS_STATEMENT
                 // note: the US spelling of licenSe is expected for the dublin core standard, so don't fix it
-                it.license = DEFAULT_LICENCE
+                it.license = it.licenceDescription ?: DEFAULT_LICENCE
             }
         }
 
