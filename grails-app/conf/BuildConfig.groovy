a67321060afcfc7b2f34b1d8cfaef61068efa036
Chris <chris.godwin.ala@gmail.com>
2020-06-07 12:05:51 +1000

    Testing a mongo java driver update.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -81,7 +81,7 @@ grails.project.dependency.resolution = {
         compile 'org.codehaus.jackson:jackson-core-asl:1.9.13'
         compile 'org.codehaus.jackson:jackson-mapper-asl:1.9.13'
 
-        compile 'org.mongodb:mongo-java-driver:2.12.3'
+        compile 'org.mongodb:mongo-java-driver:2.13.3'
 
         compile 'com.twelvemonkeys.imageio:imageio-jpeg:3.3.2'
 
