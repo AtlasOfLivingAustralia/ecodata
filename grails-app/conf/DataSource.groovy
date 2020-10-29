5471caf23d62c663b499891eb9751271ebf162b2
Thakur Adhikari <adh010@csiro.au>
2020-10-30 07:51:35 +1100

    adding replica set in grails 2 for testing purposes

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -45,7 +45,7 @@ environments {
                 host = "ecodata-test.ala.org.au"
                 port = "27017"
                 databaseName = "ecodata"
-                replicaSet = ["ecodata-reporting.ala.org.au:27017"]
+                replicaSet = "ecodata-reporting.ala.org.au:27017"
 
                 options {
                     autoConnectRetry = true
