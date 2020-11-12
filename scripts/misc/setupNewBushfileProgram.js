d29c1188710eeb62fb56bae4be9a649ebf92e8bf
Chris <chris.godwin.ala@gmail.com>
2020-11-12 11:29:20 +1100

    Setup  outcomes for new program #2103

--- scripts/misc/setupNewBushfileProgram.js
+++ scripts/misc/setupNewBushfileProgram.js
@@ -222,3 +222,75 @@ program.config.meriPlanContents = [
         }
     }
 ];
+program.outcomes = [
+    {
+        "priorities": [
+            {
+                "category": "Ramsar"
+            }
+        ],
+
+        "type":"secondary",
+        "targeted": true,
+        "shortDescription": "Ramsar Sites",
+        "category": "environment",
+        "outcome": "1. By 2023, there is restoration of, and reduction in threats to, the ecological character of Ramsar sites, through the implementation of priority actions"
+    },
+    {
+        "priorities": [
+            {
+                "category": "Threatened Species"
+            }
+        ],
+        "targeted": true,
+        "shortDescription": "Threatened Species Strategy",
+        "category": "environment",
+        "outcome": "2. By 2023, the trajectory of species targeted under the Threatened Species Strategy, and other EPBC Act priority species, is stabilised or improved."
+    },
+    {
+        "priorities": [
+            {
+                "category": "World Heritage Sites"
+            }
+        ],
+        "targeted": true,
+        "shortDescription": "World Heritage Areas",
+        "category": "environment",
+        "outcome": "3. By 2023, invasive species management has reduced threats to the natural heritage Outstanding Universal Value of World Heritage properties through the implementation of priority actions."
+    },
+    {
+        "priorities": [
+            {
+                "category": "Threatened Ecological Communities"
+            }
+        ],
+        "targeted": true,
+        "shortDescription": "Threatened Ecological Communities",
+        "category": "environment",
+        "outcome": "4. By 2023, the implementation of priority actions is leading to an improvement in the condition of EPBC Act listed Threatened Ecological Communities."
+    },
+    {
+        "priorities": [
+            {
+                "category": "Land Management"
+            }
+        ],
+        "type":"secondary",
+        "targeted": true,
+        "shortDescription": "Soil Condition",
+        "category": "agriculture",
+        "outcome": "5. By 2023, there is an increase in the awareness and adoption of land management practices that improve and protect the condition of soil, biodiversity and vegetation."
+    },
+    {
+        "priorities": [
+            {
+                "category": "Sustainable Agriculture"
+            }
+        ],
+        "type":"secondary",
+        "shortDescription": "Climate / Weather Adaption",
+        "category": "agriculture",
+        "outcome": "6. By 2023, there is an increase in the capacity of agriculture systems to adapt to significant changes in climate and market demands for information on provenance and sustainable production."
+    }
+];
+db.program.save(program);
