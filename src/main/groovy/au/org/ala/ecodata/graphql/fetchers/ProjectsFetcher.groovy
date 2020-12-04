7add5268340dba44913be8b301ba7c14d058c337
yasima-csiro <yasima.kankanamge@csiro.au>
2020-12-04 12:22:10 +1100

    Add project search API

--- src/main/groovy/au/org/ala/ecodata/graphql/fetchers/ProjectsFetcher.groovy
+++ src/main/groovy/au/org/ala/ecodata/graphql/fetchers/ProjectsFetcher.groovy
@@ -3,8 +3,11 @@ package au.org.ala.ecodata.graphql.fetchers
 import au.org.ala.ecodata.*
 import com.mongodb.client.FindIterable
 import com.mongodb.client.model.Filters
+import graphql.GraphQLException
 import graphql.schema.DataFetchingEnvironment
 import org.elasticsearch.action.search.SearchResponse
+
+import static au.org.ala.ecodata.ElasticIndex.HOMEPAGE_INDEX
 
 class ProjectsFetcher implements graphql.schema.DataFetcher<List<Project>> {
 
@@ -20,6 +23,10 @@ class ProjectsFetcher implements graphql.schema.DataFetcher<List<Project>> {
 
     ProjectService projectService
 
+    static String meritFacets = "status,organisationFacet,associatedProgramFacet,associatedSubProgramFacet,mainThemeFacet,stateFacet,nrmFacet,lgaFacet,mvgFacet,ibraFacet,imcra4_pbFacet,otherFacet,electFacet,meriPlanAssetFacet," +
+            "cmzFacet,partnerOrganisationTypeFacet,promoteOnHomepage,custom.details.caseStudy,primaryOutcomeFacet,secondaryOutcomesFacet,muFacet,tags,fundingSourceFacet"
+    static Map meritParams = [hubFq:"isMERIT:true", controller:"search", flimit:1500, fsort:"term", query:"docType:project", action:"elasticHome", facets:meritFacets, format:null]
+
     @Override
     List<Project> get(DataFetchingEnvironment environment) throws Exception {
 
@@ -34,19 +41,19 @@ class ProjectsFetcher implements graphql.schema.DataFetcher<List<Project>> {
         // e.g. should the role check only apply during the mapping phase?  In which case we need a bulk query of permissions to determine a list of project ids we can get full resolution data for?
         // Or do we do two queries, one for full resolution, one for the rest (how do we sort/page if we do two queries?)
 
+        String query = environment.arguments.term ?:"*:*"
 
-
-        return queryElasticSearch(environment)
+        return queryElasticSearch(environment, query, [include:'projectId'])
     }
 
-    private List<Project> queryElasticSearch(DataFetchingEnvironment environment) {
+    private List<Project> queryElasticSearch(DataFetchingEnvironment environment, String queryString, Map params) {
         // Retrieve projectIds only from elasticsearch.
 
 
         // add pagination results.
         String userId = environment.context.userId ?: '1493'
-        String query = environment.arguments.term ?:"*:*"
-        SearchResponse searchResponse = elasticSearchService.searchWithSecurity(userId, query, [include:'projectId'], ElasticIndex.HOMEPAGE_INDEX)
+        String query = queryString ?:"*:*"
+        SearchResponse searchResponse = elasticSearchService.searchWithSecurity(userId, query, params, HOMEPAGE_INDEX)
 
         List<String> projectIds = searchResponse.hits.hits.collect{it.source.projectId}
 
@@ -82,5 +89,96 @@ class ProjectsFetcher implements graphql.schema.DataFetcher<List<Project>> {
         }
 
         projects
+    }
+
+    List<Project> searchMeritProject (DataFetchingEnvironment environment) {
+
+        def fqList = []
+        def facetMappings = ["managementArea": "nrmFacet:", "majorVegetationGroup": "mvgFacet:", "biogeographicRegion": "ibraFacet:", "marineRegion": "imcra4_pbFacet:", "otherRegion": "otherFacet:", "grantManagerNominatedProject":"promoteOnHomepage:",
+                             "federalElectorate": "electFacet:", "assetsAddressed": "meriPlanAssetFacet:", "userNominatedProject": "custom.details.caseStudy:", "managementUnit": "muFacet:"]
+
+        environment.arguments.each {
+            if(it.key in ["fromDate", "toDate", "dateRange"]) {
+                return
+            }
+
+            String key
+
+            it.value.each { val ->
+                switch (it.key) {
+                    case "status":
+                    case "tags":
+                        key = it.key + ":"
+                        break;
+                    case "managementArea" :
+                    case "majorVegetationGroup" :
+                    case "biogeographicRegion" :
+                    case "marineRegion" :
+                    case "otherRegion" :
+                    case "grantManagerNominatedProject" :
+                    case "federalElectorate" :
+                    case "assetsAddressed" :
+                    case "userNominatedProject" :
+                    case "managementUnit" :
+                        key = facetMappings.get(it.key)
+                        break;
+                    default:
+                        key = it.key + "Facet:"
+                        break;
+                }
+                fqList << key + val;
+            }
+        }
+
+        //validate the query
+        validateSearchQuery(environment, fqList)
+
+        Map params = meritParams
+        params["fq"] = fqList
+
+        if(environment.arguments.get("fromDate")) {
+            params["fromDate"] = environment.arguments.get("fromDate").toString()
+        }
+
+        if(environment.arguments.get("toDate")) {
+            params["toDate"] = environment.arguments.get("toDate").toString()
+        }
+
+        if(environment.arguments.get("dateRange")) {
+
+            params["fromDate"] = environment.arguments.get("dateRange").from
+            params["toDate"] = environment.arguments.get("dateRange").to
+        }
+
+        return queryElasticSearch(environment, "docType: project", params)
+    }
+
+    void validateSearchQuery (DataFetchingEnvironment environment, List fqList) {
+
+        def searchDetails = elasticSearchService.search("docType: project", meritParams, HOMEPAGE_INDEX)
+
+        fqList.each {
+            List fq = it.toString().split(":")
+            List<String> lookUps = searchDetails.facets.getFacets().get(fq.first()).entries.term as String[]
+            if(!lookUps.contains(fq.last())) {
+                throw new GraphQLException('Invalid ' +  fq.first() +' : suggested values are : ' + lookUps)
+            }
+        }
+
+        def datePattern = /\d{4}\-\d{2}\-\d{2}/
+
+        //validate the format of the from anf to dates
+        if(environment.arguments.get("fromDate")) {
+            if(!(environment.arguments.get("fromDate") ==~ datePattern)){
+                throw new GraphQLException('Invalid fromDate: fromDate should match yyyy-mm-dd')
+            }
+        }
+
+        if(environment.arguments.get("toDate")) {
+            if(!(environment.arguments.get("fromDate") ==~ datePattern)){
+                throw new GraphQLException('Invalid toDate: toDate should match yyyy-mm-dd')
+            }
+        }
+
     }
 }
