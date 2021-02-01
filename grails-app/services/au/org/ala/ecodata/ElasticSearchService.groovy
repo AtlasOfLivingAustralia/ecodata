bc48a3b06bbde06cac752c6d7dacdd7186a94fbe
Chris <chris.godwin.ala@gmail.com>
2021-02-01 13:57:59 +1100

    Added debug statement for #634

--- grails-app/services/au/org/ala/ecodata/ElasticSearchService.groovy
+++ grails-app/services/au/org/ala/ecodata/ElasticSearchService.groovy
@@ -167,6 +167,7 @@ class ElasticSearchService {
             builder.setSource(docJson.toString(false)).execute().actionGet()
 
         } catch (Exception e) {
+            e.printStackTrace()
             log.error "Error indexing document: ${docJson.toString(true)}\nError: ${e}", e
 
             String subject = "Indexing failed on server ${grailsApplication.config.grails.serverURL}"
