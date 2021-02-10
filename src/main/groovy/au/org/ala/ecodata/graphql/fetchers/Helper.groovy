875b0a88049ae887e7e19e7ca18104175faf4e19
yasima-csiro <yasima.kankanamge@csiro.au>
2021-02-10 12:54:30 +1100

    Add form versioning

--- src/main/groovy/au/org/ala/ecodata/graphql/fetchers/Helper.groovy
+++ src/main/groovy/au/org/ala/ecodata/graphql/fetchers/Helper.groovy
@@ -85,6 +85,7 @@ class Helper {
         ActivityForm.findAllWhereStatusNotEqualAndPublicationStatusEquals(Status.DELETED, PublicationStatus.PUBLISHED).each { ActivityForm activityForm ->
             Map activityModel = [
                     name: activityForm.name,
+                    formVersion: activityForm.formVersion,
                     outputs: []
             ]
 
