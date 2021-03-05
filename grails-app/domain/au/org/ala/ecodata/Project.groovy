869fbcb3cabc3d4954d0faffd29253c6808c071e
Chris <chris.godwin.ala@gmail.com>
2021-03-05 16:19:10 +1100

    Fixed bad merge with duplicate terminationReason #644

--- grails-app/domain/au/org/ala/ecodata/Project.groovy
+++ grails-app/domain/au/org/ala/ecodata/Project.groovy
@@ -86,7 +86,6 @@ class Project {
     MapLayersConfiguration mapLayersConfig
     /** configure how activity is displayed on map for example point, heatmap or cluster. */
     List mapDisplays
-    String terminationReason
 
     boolean alaHarvest = false
     //For embedded table, needs to conversion in controller
