35bc104996b496ff0e462886db10725771f2adcc
yasima-csiro <yasima.kankanamge@csiro.au>
2021-03-09 13:45:50 +1100

    Add MU API

---
+++ src/main/groovy/au/org/ala/ecodata/graphql/fetchers/ManamgementUnitFetcher.groovy
@@ -0,0 +1,41 @@
+package au.org.ala.ecodata.graphql.fetchers
+
+import au.org.ala.ecodata.*
+import au.org.ala.ecodata.graphql.models.Schema
+import au.org.ala.ecodata.graphql.models.Summary
+import grails.core.GrailsApplication
+import grails.util.Holders
+import graphql.schema.DataFetchingEnvironment
+import org.apache.commons.lang.WordUtils
+import org.elasticsearch.action.search.SearchResponse
+import org.springframework.context.MessageSource
+
+import static au.org.ala.ecodata.Status.ACTIVE
+
+class ManamgementUnitFetcher implements graphql.schema.DataFetcher<List<Activity>> {
+
+    @Override
+    List<ManagementUnit> get(DataFetchingEnvironment environment) throws Exception {
+
+        String muId = environment.arguments.managementUnitId
+        String name = environment.arguments.name
+        String startDate = environment.arguments.startDate
+        String endDate = environment.arguments.endDate
+
+        def list = ManagementUnit.createCriteria().list() {
+            if(muId){
+                eq ("managementUnitId", muId)
+            }
+            if(name){
+                like ("name", "%" + name + "%")
+            }
+            if(startDate){
+                ge ("startDate", Date.parse("yyyy-MM-dd", startDate))
+            }
+            if(endDate){
+                ge ("endDate", Date.parse("yyyy-MM-dd", endDate))
+            }
+        }
+        return list
+    }
+}
