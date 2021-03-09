6d807ab28304a1dcc979ca91801506ff6447b9e3
yasima-csiro <yasima.kankanamge@csiro.au>
2021-03-09 13:45:50 +1100

    Add MU API

--- grails-app/controllers/au/org/ala/ecodata/GraphqlInterceptor.groovy
+++ grails-app/controllers/au/org/ala/ecodata/GraphqlInterceptor.groovy
@@ -1,6 +1,7 @@
 package au.org.ala.ecodata
 
 import au.org.ala.web.AuthService
+import au.org.ala.web.CASRoles
 import grails.converters.JSON
 import au.org.ala.web.UserDetails
 
@@ -10,88 +11,42 @@ class GraphqlInterceptor {
     PermissionService permissionService
     AuthService authService
 
-    def LOCALHOST_IP = '127.0.0.1'
-
     GraphqlInterceptor() {
-        match uri: '/graphql/index/**'
+        match uri: '/graphql/**'
     }
 
     boolean before() {
-        //validate the username and given authKey
-        String userName = request.getHeader(grailsApplication.config.app.http.header.userId)
-        String authKey = request.getHeader('authKey')
+        String userName = request.getHeader(grailsApplication.config.app.http.header.userId) ?:
+                request.cookies.find { it.name == 'ALA-Auth' }.value
 
-        if(authKey && userName) {
+        if (userName) {
+            //test to see that the user is valid
+            UserDetails user = authService.getUserForEmailAddress(userName)
 
-            def result = userService.validateKey(userName, authKey)
-            if (!result?.resp?.statusCode && result.resp?.status == 'success') {
-                //and also, only the ala admins are permitted to do graphql searches
-                if (permissionService.isUserAlaAdmin(userName)) {
+            if(!user){
+                accessDeniedError('Invalid GrapqhQl API usage: Access denied, userId: ' + userName)
+                return false
+            }
+            else{
+                //TODO add Biocollect hub owners roles
+                if(permissionService.isUserAlaAdmin(userName) || userService.getRolesForUser(userName)?.contains("ROLE_FC_ADMIN")) {
                     return true
                 }
-                else{
+                else {
                     accessDeniedError('Invalid GrapqhQl API usage: Access denied, userId: ' + userName)
                     return false
                 }
-            } else {
-                accessDeniedError('Invalid GrapqhQl API usage: Invalid username or api key, userId: ' + userName)
-                return false
             }
         }
-        // This is to validate graphql browser user
         else{
-            def userCookie = null
-
-            if(request.cookies) {
-                userCookie = request.cookies.find { it.name == 'ALA-Auth' }
-            }
-            UserDetails user = null
-
-            if (userCookie) {
-                String username = URLDecoder.decode(userCookie.getValue(), 'utf-8')
-                //test to see that the user is valid
-                user = authService.getUserForEmailAddress(username)
-
-                if(!user){
-                    accessDeniedError('Invalid GrapqhQl API usage: Access denied, userId: ' + username)
-                    return false
-                }
-                else{
-                    def whiteList = buildWhiteList()
-                    def clientIp = request.getHeader("X-Forwarded-For") ?: request.getRemoteHost()
-                    def ipOk = whiteList.contains(clientIp) || (whiteList.size() == 1 && whiteList[0] == LOCALHOST_IP)
-
-                    if(ipOk && permissionService.isUserAlaAdmin(username)) {
-                        userService.setCurrentUser(username)
-                        return true
-                    }
-                    else {
-                        accessDeniedError('Invalid GrapqhQl API usage: Access denied, userId: ' + username)
-                        return false
-                    }
-                }
-            }
-            else{
-                accessDeniedError('Invalid GrapqhQl API usage: Access denied')
-                return false
-            }
-
-            false
+            accessDeniedError('Invalid GrapqhQl API usage: No user Id')
+            return false
         }
-    }
+}
 
     boolean after = { }
 
     void afterView() { }
-
-    def buildWhiteList() {
-        def whiteList = [LOCALHOST_IP]
-        def config = grailsApplication.config.app.api.whiteList as String
-        if (config) {
-            whiteList.addAll(config.split(',').collect({it.trim()}))
-        }
-        whiteList
-    }
 
     def accessDeniedError(String error) {
         Map map = [error: 'Access denied', status: 401]
