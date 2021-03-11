4bd3fab43fa5e86df2a36bc6381f5f041b5c61e3
yasima-csiro <yasima.kankanamge@csiro.au>
2021-03-12 07:58:30 +1100

    Fix test cases

--- src/integration-test/groovy/au/org/ala/ecodata/GraphqlSpecHelper.groovy
+++ src/integration-test/groovy/au/org/ala/ecodata/GraphqlSpecHelper.groovy
@@ -1,17 +1,55 @@
 package au.org.ala.ecodata
 
-import org.grails.gorm.graphql.plugin.testing.GraphQLSpec
+import grails.util.GrailsWebMockUtil
+import groovy.transform.CompileStatic
+import org.grails.plugins.testing.GrailsMockHttpServletRequest
+import org.grails.plugins.testing.GrailsMockHttpServletResponse
+import org.grails.web.servlet.mvc.GrailsWebRequest
+import org.junit.Ignore
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletResponse
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.context.request.RequestContextHolder
 import spock.lang.Specification
 
-class GraphqlSpecHelper extends Specification implements GraphQLSpec{
+@CompileStatic
+abstract class GraphqlSpecHelper extends Specification {
 
-    def grailsApplication
+    @Autowired
+    WebApplicationContext ctx
 
-    def graphqlRequest(String requestBody, String userName){
-        return rest.post("http://${grailsApplication.config.grails.hostname}:${serverPort}/graphql/index") {
-            contentType("application/graphql")
-            header("Cookie", "ALA-Auth=" + userName);
-            body(requestBody)
-        }
+    void setup() {
+        MockHttpServletRequest request = new GrailsMockHttpServletRequest(ctx.servletContext)
+        MockHttpServletResponse response = new GrailsMockHttpServletResponse()
+        GrailsWebMockUtil.bindMockWebRequest(ctx, request, response)
+        currentRequestAttributes.setControllerName(controllerName)
+    }
+
+    @Ignore
+    abstract String getControllerName()
+
+    @Ignore
+    protected GrailsWebRequest getCurrentRequestAttributes() {
+        return (GrailsWebRequest) RequestContextHolder.currentRequestAttributes()
+    }
+
+    void cleanup() {
+        RequestContextHolder.resetRequestAttributes()
+    }
+
+    @Ignore
+    def autowire(Class clazz) {
+        def bean = clazz.newInstance()
+        ctx.autowireCapableBeanFactory.autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false)
+        bean
+    }
+
+
+    @Ignore
+    def autowire(def bean) {
+        ctx.autowireCapableBeanFactory.autowireBeanProperties(bean, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false)
+        bean
     }
 }
