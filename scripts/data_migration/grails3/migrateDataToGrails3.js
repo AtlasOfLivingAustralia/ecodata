b997f3267730b1e50a36e1042f2be921c83169d9
Chris <chris.godwin.ala@gmail.com>
2021-07-27 13:49:26 +1000

    Removed completed data migration #640

--- scripts/data_migration/grails3/migrateDataToGrails3.js
+++ scripts/data_migration/grails3/migrateDataToGrails3.js
@@ -1,7 +1,6 @@
 load("./unsetUserPermissionVersion.js");
 load("./changeReportDoubleToIntegers.js");
 load("./updateEmptyGeoIndex.js");
-load("./updateScoreValueId.js");
 load("./updateUndefinedValues.js");
 load("./updateDocumentDateTaken.js");
 load("./updateProgramConfig.js");
