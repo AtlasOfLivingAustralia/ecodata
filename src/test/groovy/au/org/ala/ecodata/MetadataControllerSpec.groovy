6d28caa91daff3788cbaee4d9e631e63d9bba0f3
yasima-csiro <yasima.kankanamge@csiro.au>
2021-08-18 15:00:29 +1000

    Fix test #681

--- src/test/groovy/au/org/ala/ecodata/MetadataControllerSpec.groovy
+++ src/test/groovy/au/org/ala/ecodata/MetadataControllerSpec.groovy
@@ -1,16 +1,24 @@
 package au.org.ala.ecodata
 
+import au.org.ala.ecodata.util.ExportTestUtils
 import grails.testing.gorm.DataTest
 import grails.testing.web.controllers.ControllerUnitTest
 import org.apache.http.HttpStatus
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Workbook
 import spock.lang.Specification
 
 class MetadataControllerSpec extends Specification implements ControllerUnitTest<MetadataController>, DataTest {
 
     MetadataService metadataService = Mock(MetadataService)
+    File outputFile
+    ExcelImportService excelImportService
+
 
     def setup() {
         controller.metadataService = metadataService
+        outputFile = File.createTempFile('test', '.xlsx')
+        excelImportService = new ExcelImportService()
     }
 
     def "Get excel template that can be populated with output data and uploaded"() {
@@ -33,6 +41,15 @@ class MetadataControllerSpec extends Specification implements ControllerUnitTest
         response.contentType == "application/vnd.ms-excel"
         response.headerNames.contains("Content-disposition")
         response.header("Content-disposition") == 'attachment; filename="test.xlsx";'
+
+        OutputStream outStream = new FileOutputStream(outputFile)
+        outStream.setBytes(response.contentAsByteArray)
+        Workbook workbook =  ExportTestUtils.readWorkbook(outputFile)
+        Sheet sheet = workbook.getSheet(outputName)
+        sheet.physicalNumberOfRows == 1 //header row
+        sheet.head().getPhysicalNumberOfCells() == 1 //one field
+        sheet.head().getCell(0).toString() == 'testField'
+
     }
 
 }
