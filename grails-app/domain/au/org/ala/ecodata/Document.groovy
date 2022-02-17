76a5bc5507b5078de490bdcdc167ca802acebd05
yasima-csiro <yasima.kankanamge@csiro.au>
2022-02-17 13:34:58 +1100

    fix merge issue

--- grails-app/domain/au/org/ala/ecodata/Document.groovy
+++ grails-app/domain/au/org/ala/ecodata/Document.groovy
@@ -59,7 +59,6 @@ class Document {
     String outputId
     String organisationId
     String programId
-    String managementUnitId
     String reportId
     String managementUnitId
     String hubId
