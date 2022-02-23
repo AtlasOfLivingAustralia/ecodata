ce4368aec64105fe14c6ca5c227cab393c9d71b7
yasima-csiro <yasima.kankanamge@csiro.au>
2022-02-23 13:57:04 +1100

    Fix bushfire surveyor script

--- scripts/models/bushfireSurveyor/bushfireSurveyor.js
+++ scripts/models/bushfireSurveyor/bushfireSurveyor.js
@@ -1,21 +1,21 @@
 self.data.recordType = ko.observable().extend({metadata:{metadata:self.dataModel['recordType'], context:self.$context, config:config}});
 
-$('[data-bind="click:data.propertyInformationRepeatSection.addRow"]').parent().hide()
-$('[data-bind="click:data.bushInformationRepeatSection.addRow"]').parent().hide()
+$('div[data-bind*="propertyInformationRepeatSection"]').parent().parent().hide()
+$('div[data-bind*="bushInformationRepeatSection"]').parent().parent().hide()
 
 self.data.recordType.subscribe(function(obj) {
     var recordType =self.data.recordType();
 
     if(recordType == "Property (house and garden)"){
-        $('[data-bind="click:data.propertyInformationRepeatSection.addRow"]').parent().show()
-        $('[data-bind="click:data.bushInformationRepeatSection.addRow"]').parent().hide()
+        $('div[data-bind*="propertyInformationRepeatSection"]').parent().parent().show()
+        $('div[data-bind*="bushInformationRepeatSection"]').parent().parent().hide()
     }
     else if(recordType == "Bushland"){
-        $('[data-bind="click:data.propertyInformationRepeatSection.addRow"]').parent().hide()
-        $('[data-bind="click:data.bushInformationRepeatSection.addRow"]').parent().show()
+        $('div[data-bind*="propertyInformationRepeatSection"]').parent().parent().hide()
+        $('div[data-bind*="bushInformationRepeatSection"]').parent().parent().show()
     }
     else{
-        $('[data-bind="click:data.propertyInformationRepeatSection.addRow"]').parent().hide()
-        $('[data-bind="click:data.bushInformationRepeatSection.addRow"]').parent().hide()
+        $('div[data-bind*="propertyInformationRepeatSection"]').parent().parent().hide()
+        $('div[data-bind*="bushInformationRepeatSection"]').parent().parent().hide()
     }
 });
