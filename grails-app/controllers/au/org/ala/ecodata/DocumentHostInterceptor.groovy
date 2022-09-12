86d37ea8b976b9f1f3aa8f28c9a200bb1f634f4d
Chris <chris.godwin.ala@gmail.com>
2022-09-12 17:18:36 +1000

    Replaced grails config access with getProperty #764

--- grails-app/controllers/au/org/ala/ecodata/DocumentHostInterceptor.groovy
+++ grails-app/controllers/au/org/ala/ecodata/DocumentHostInterceptor.groovy
@@ -8,25 +8,17 @@ import org.springframework.web.context.request.RequestAttributes
 class DocumentHostInterceptor {
     static final String DOCUMENT_HOST_NAME = "DOCUMENT_HOST_NAME"
 
-    /** The HTTP header to look for the host name */
-    private String hostNameHeader
-
-    /** Allowed domain suffix for the hast name */
-    private String allowedHostSuffix
-
     HubService hubService
     DocumentHostInterceptor () {
         matchAll()
-        hostNameHeader = grailsApplication.config.getProperty('app.http.header.hostName')
-        allowedHostSuffix = grailsApplication.config.getProperty('app.allowedHostName')
     }
 
     boolean before() {
-        String hostName = request.getHeader(hostNameHeader)
+        String hostName = request.getHeader(grailsApplication.config.getProperty('app.http.header.hostName'))
         if (hostName) {
             try {
                 URI host = new URI(hostName)
-                if (host.scheme && host.host?.endsWith(allowedHostSuffix)) {
+                if (host.scheme && host.host?.endsWith(grailsApplication.config.getProperty('app.allowedHostName'))) {
                     hostName = "${host.scheme}://${host.host}${host.port != -1?':' + host.port : ''}"
                     GrailsWebRequest.lookup().setAttribute(DOCUMENT_HOST_NAME, hostName, RequestAttributes.SCOPE_REQUEST)
                 }
