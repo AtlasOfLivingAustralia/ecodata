d26139ac1100d9aa2637bf5c37347abace3b4c2d
temi <temi.varghese@csiro.au>
2023-04-20 09:42:49 +1000

    adding organisation to export

--- scripts/reports/IBSAProjectPublicResources.js
+++ scripts/reports/IBSAProjectPublicResources.js
@@ -6,10 +6,10 @@ db.project.find({associatedProgram: {$in: programs}, status: "active"}).forEach(
 });
 
 var projects = db.project.distinct('projectId', {associatedProgram: {$in: programs}, status:{$ne:'deleted'}});
-print("Project id,Project name,Document id,Title,Description,Document type,File size,Content type,URL");
+print("Project id,Project name,Organisation,Document id,Title,Description,Document type,File size,Content type,URL");
 db.document.find({projectId: {$in: projects}, "public": true, status:{$ne:'deleted'}}).forEach(function(document) {
     var project = db.project.find({projectId: document.projectId}).next();
     var path = document.filepath ? document.filepath + "/" : "";
     var url = "https://biocollect.ala.org.au/document/download/" + path + encodeURIComponent(document.filename);
-    print(document.projectId + ",\"" + project.name.replace('"', '""') + "\"," + document.documentId + "," + document.name + ",\"" + (document.description || "").replace('"', '""') + "\"," + document.type + "," + document.filesize + "," + document.contentType + "," + url);
+    print(document.projectId + ",\"" + project.name.replace('"', '""') + "\",\"" + project.organisationName.replace('"', '""') + "\"," + document.documentId + "," + document.name + ",\"" + (document.description || "").replace('"', '""') + "\"," + document.type + "," + document.filesize + "," + document.contentType + "," + url);
 });
