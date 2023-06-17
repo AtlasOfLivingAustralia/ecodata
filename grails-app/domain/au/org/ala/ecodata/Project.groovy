baf78843d3d06ecf10d5f235f4597e4cc7ecbdab
Chris <chris.godwin.ala@gmail.com>
2023-06-18 09:38:08 +1000

    Fix protocol assignment for paratoo #823

--- grails-app/domain/au/org/ala/ecodata/Project.groovy
+++ grails-app/domain/au/org/ala/ecodata/Project.groovy
@@ -282,10 +282,10 @@ class Project {
 
     /** Used to find relevant TERN/paratoo monitoring protocols for a project */
     List<String> getMonitoringProtocolCategories() {
-        List baselineProtocols = custom?.details?.baseline?.rows?.collect{it.protocols}?.flatten()
-        List monitoringProtocols = custom?.details?.monitoring?.rows?.collect{it.protocols}?.flatten()
+        List baselineProtocols = custom?.details?.baseline?.rows?.collect{it.protocols}?.flatten() ?:[]
+        List monitoringProtocols = custom?.details?.monitoring?.rows?.collect{it.protocols}?.flatten() ?:[]
 
-        (baselineProtocols ?: [] + monitoringProtocols ?:[] ).unique().findAll{it}
+        (baselineProtocols + monitoringProtocols).unique().findAll{it}
     }
 
 }
