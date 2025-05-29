7dc4e972f9c2017128adca12f43c4f11451446b5
chrisala <chris.godwin.ala@gmail.com>
2025-05-30 09:36:00 +1000

    Fixed activity query to stop log messages from deleted forms #1108

--- grails-app/services/au/org/ala/ecodata/MetadataService.groovy
+++ grails-app/services/au/org/ala/ecodata/MetadataService.groovy
@@ -68,7 +68,7 @@ class MetadataService implements DataBinder {
         Map maxVersionsByName = [:]
         Map activitiesByName = [:]
 
-        ActivityForm.findAllWhereStatusNotEqualAndPublicationStatusEquals(Status.DELETED, PublicationStatus.PUBLISHED).each { ActivityForm activityForm ->
+        ActivityForm.findByStatusNotEqualAndPublicationStatus(Status.DELETED, PublicationStatus.PUBLISHED).each { ActivityForm activityForm ->
             Map activityModel = [
                     name: activityForm.name,
                     gmsId: activityForm.gmsId,
