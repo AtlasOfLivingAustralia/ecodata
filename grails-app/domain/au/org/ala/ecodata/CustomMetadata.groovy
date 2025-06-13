276905b6bb09ceef7667d3b28b3cffbdde767abb
sal153 <joseph.salomon@csiro.au>
2025-06-13 12:14:49 +1000

    commit code review updates #1707

--- grails-app/domain/au/org/ala/ecodata/CustomMetadata.groovy
+++ grails-app/domain/au/org/ala/ecodata/CustomMetadata.groovy
@@ -1,15 +1,11 @@
 package au.org.ala.ecodata
 
 class CustomMetadata {
-    String raid
     String category
-    String nationalScale
 
     Map<String, Object> toMap() {
         [
-            nespRaid: raid,
-            nespCategory: category,
-            nespNationalScale: nationalScale
+            nespCategory: category
         ]
     }
 }
