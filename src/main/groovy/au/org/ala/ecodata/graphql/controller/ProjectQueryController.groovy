3318439562f709ef97e888dedb2b029c0ba1dece
chrisala <chris.godwin.ala@gmail.com>
2025-09-22 08:01:07 +1000

    Fixed review issue #1117

--- src/main/groovy/au/org/ala/ecodata/graphql/controller/ProjectQueryController.groovy
+++ src/main/groovy/au/org/ala/ecodata/graphql/controller/ProjectQueryController.groovy
@@ -54,7 +54,7 @@ class ProjectQueryController implements DataBinder {
 
 
     @QueryMapping
-    Map<Integer, List<Project>> searchMeritProjects(DataFetchingEnvironment env) {
+    Map<String, Object> searchMeritProjects(DataFetchingEnvironment env) {
         Map hub = (Map)env.graphQlContext.get("hub")
         if (hub.urlPath != "merit") {
             throw new IllegalArgumentException("The searchMeritProjects query is only available via the MERIT hub path")
