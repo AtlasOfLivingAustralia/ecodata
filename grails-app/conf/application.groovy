52b9aa7b6979b348cf078a0bce8218d043ef3cbe
chrisala <chris.godwin.ala@gmail.com>
2026-08-26 11:49:13 +1000

    Moved M2M check to UserService for use in Monitor API #1284

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -2035,4 +2035,4 @@ paratoo.defaultPlotLayoutViewModels = [
                 ]
         ]
 paratoo.species.specialCases = ["Other", "N/A"]
-paratoo.api.writeScope = "paratoo_api_write"
+paratoo.api.writeScope = "ecodata/write_paratoo_api_test"
